8517421facbf3f6d2d52cb1cceb12f2ec64192e6
Vojtech Pacik <vojtech.pacik@cern.ch>
2016-09-05 16:05:13 +0200

    FlowPID: starting from scratch

---
+++ flow/AliAnalysisTaskFlowPID.h
@@ -0,0 +1,32 @@
+/* Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. */
+/* See cxx source for full Copyright notice */
+/* $Id$ */
+
+#ifndef AliAnalysisTaskFlowPID_H
+#define AliAnalysisTaskFlowPID_H
+
+#include "AliAnalysisTaskSE.h"
+
+class AliAnalysisTaskFlowPID : public AliAnalysisTaskSE  
+{
+    public:
+                                AliAnalysisTaskFlowPID();
+                                AliAnalysisTaskFlowPID(const char *name);
+        virtual                 ~AliAnalysisTaskFlowPID();
+
+        virtual void            UserCreateOutputObjects();
+        virtual void            UserExec(Option_t* option);
+        virtual void            Terminate(Option_t* option);
+
+    private:
+        AliAODEvent*            fAOD;           //! input event
+        TList*                  fOutputList;    //! output list
+        TH1F*                   fHistPt;        //! dummy histogram
+
+        AliAnalysisTaskFlowPID(const AliAnalysisTaskFlowPID&); // not implemented
+        AliAnalysisTaskFlowPID& operator=(const AliAnalysisTaskFlowPID&); // not implemented
+
+        ClassDef(AliAnalysisTaskFlowPID, 1);
+};
+
+#endif
