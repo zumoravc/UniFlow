eada32a294e3982c88f78d6a1fb52bb36d9673da
Vojtech Pacik <vojtech.pacik@cern.ch>
2016-09-28 09:51:21 +0200

    Task.h: cuts bug fixed: removed streamer from non trasient data members

--- flow/AliAnalysisTaskFlowPID.h
+++ flow/AliAnalysisTaskFlowPID.h
@@ -50,14 +50,14 @@ class AliAnalysisTaskFlowPID : public AliAnalysisTaskSE
         void										SetV0sProtonNumSigmaMax(Double_t nSigma) { fCutV0ProtonNumSigmaMax = nSigma; }
 
 
-        const static Int_t 			fNumPtBins = 10;			//! number of pT bins used for pT-differential flow
-        static Double_t					fPtBinEdges[fNumPtBins+1];				//! pointer for array of pT bin edges
-        const static Int_t      fNumMinvFlowBinsK0s = 12;  //! number of inv. mass bin for differential flow plots (K0s)
+        const static Int_t 			fNumPtBins = 10;			// number of pT bins used for pT-differential flow
+        static Double_t					fPtBinEdges[fNumPtBins+1];				// pointer for array of pT bin edges
+        const static Int_t      fNumMinvFlowBinsK0s = 12;  // number of inv. mass bin for differential flow plots (K0s)
         static Double_t         fMinvFlowBinEdgesK0s[fNumMinvFlowBinsK0s+1]; // pointer to array of Minv bin edges (K0s)
-        const static Int_t      fNumMinvFlowBinsLambda = 9;  //! number of inv. mass bin for differential flow plots ((A)Lambda)
+        const static Int_t      fNumMinvFlowBinsLambda = 10;  // number of inv. mass bin for differential flow plots ((A)Lambda)
         static Double_t         fMinvFlowBinEdgesLambda[fNumMinvFlowBinsLambda+1]; // pointer to array of Minv bin edges ((A)Lambda)
-        const static Int_t 			fNumCentBins = 9;			//! number of centrality bins used for pT-differential flow (so far independently of reference flow)
-        static Double_t					fCentBinEdges[fNumCentBins+1];				//! pointer for array of pT bin edges
+        const static Int_t 			fNumCentBins = 9;			// number of centrality bins used for pT-differential flow (so far independently of reference flow)
+        static Double_t					fCentBinEdges[fNumCentBins+1];				// pointer for array of pT bin edges
 
     private:
         Bool_t                  IsEventSelected(const AliAODEvent* event);
@@ -76,75 +76,75 @@ class AliAnalysisTaskFlowPID : public AliAnalysisTaskSE
         Short_t                 GetMinvFlowBinIndexLambda(const Double_t dMass);
 
         //cuts & selection: event & track
-        Bool_t									fAODAnalysis;		//! is AOD analysis?
-        Bool_t									fPbPb;					//! is PbPb analysis?
-				Bool_t       						fLHC10h;        //! flag to LHC10h data?
-				Short_t									fCentFlag;			//! centrality flag
-        Double_t 								fPVtxCutZ; 			//! PV z cut
-        Double_t 								fTrackEtaMax; 	//! Maximum pseudorapidity range
-        Double_t								fTrackPtMax;		//! Maximal track pT
-        Double_t								fTrackPtMin;		//! Manimal track pT
-        UShort_t								fNumTPCclsMin;	//! Minimal number of TPC clusters used for track reconstruction
-        UInt_t									fTrackFilterBit;//! Required track filter bit 
-        Bool_t 									fDiffFlow;			//! Do differential flow ? (or reference only)
-        Bool_t 									fPID;						//! Do PID (so far V0s) ? 
+        Bool_t									fAODAnalysis;		// is AOD analysis?
+        Bool_t									fPbPb;					// is PbPb analysis?
+				Bool_t       						fLHC10h;        // flag to LHC10h data?
+				Short_t									fCentFlag;			// centrality flag
+        Double_t 								fPVtxCutZ; 			// PV z cut
+        Double_t 								fTrackEtaMax; 	// Maximum pseudorapidity range
+        Double_t								fTrackPtMax;		// Maximal track pT
+        Double_t								fTrackPtMin;		// Manimal track pT
+        UShort_t								fNumTPCclsMin;	// Minimal number of TPC clusters used for track reconstruction
+        UInt_t									fTrackFilterBit;// Required track filter bit 
+        Bool_t 									fDiffFlow;			// Do differential flow ? (or reference only)
+        Bool_t 									fPID;						// Do PID (so far V0s) ? 
 				//cuts & selection: V0 reconstruction
-				Bool_t 									fCutV0onFly;		//! V0 reconstruction method: is On-the-fly? (or offline)
-				Bool_t									fCutV0refitTPC; //! Check TPC refit of V0 daughters ?
-				Bool_t									fCutV0rejectKinks; //! Reject Kink V0 daughter tracks ?
-				Double_t                fCutV0MinDCAtoPV;   //! min DCA of V0 daughter to PV
-        Double_t								fCutV0MaxDCAtoPV;	//! max DCA of V0 daughter to PV
-				Double_t								fCutV0MaxDCADaughters;	//! max DCA of V0 daughters among themselves
-        Double_t                fCutV0MinDecayRadius; //! min distance between PV and secondary vertex in transverse plane
-				Double_t								fCutV0MaxDecayRadius; //! max distance between PV and secondary vertex in transverse plane
-        Double_t                fCutV0DaughterPtMin; //! minimum pT of V0 daughters
-        Double_t                fCutV0DaughterEtaMax; //! max value of Eta of V0 daughters
-        Double_t                fCutV0MotherEtaMax; //! max eta value of V0 mother
-        Double_t                fCutV0MotherRapMax; //! max rapidity value of V0 mother
-        Double_t                fCutV0MinCPAK0s;    //! min cosine of pointing angle of K0s candidate to PV
-        Double_t                fCutV0MinCPALambda; //! min cosine of pointing angle of K0s candidate to PV
-        Double_t                fCutV0NumTauK0sMax; //! [tau] max number of c*tau (K0s)
-        Double_t                fCutV0NumTauLambdaMax; //! [tau] max number of c*tau ((A)Lambda)
-        Double_t								fCutV0ProtonNumSigmaMax;	//! [sigmaTPC] max number of TPC sigma for proton PID (Lambda candidates)
+				Bool_t 									fCutV0onFly;		// V0 reconstruction method: is On-the-fly? (or offline)
+				Bool_t									fCutV0refitTPC; // Check TPC refit of V0 daughters ?
+				Bool_t									fCutV0rejectKinks; // Reject Kink V0 daughter tracks ?
+				Double_t                fCutV0MinDCAtoPV;   // min DCA of V0 daughter to PV
+        Double_t								fCutV0MaxDCAtoPV;	// max DCA of V0 daughter to PV
+				Double_t								fCutV0MaxDCADaughters;	// max DCA of V0 daughters among themselves
+        Double_t                fCutV0MinDecayRadius; // min distance between PV and secondary vertex in transverse plane
+				Double_t								fCutV0MaxDecayRadius; // max distance between PV and secondary vertex in transverse plane
+        Double_t                fCutV0DaughterPtMin; // minimum pT of V0 daughters
+        Double_t                fCutV0DaughterEtaMax; // max value of Eta of V0 daughters
+        Double_t                fCutV0MotherEtaMax; // max eta value of V0 mother
+        Double_t                fCutV0MotherRapMax; // max rapidity value of V0 mother
+        Double_t                fCutV0MinCPAK0s;    // min cosine of pointing angle of K0s candidate to PV
+        Double_t                fCutV0MinCPALambda; // min cosine of pointing angle of K0s candidate to PV
+        Double_t                fCutV0NumTauK0sMax; // [tau] max number of c*tau (K0s)
+        Double_t                fCutV0NumTauLambdaMax; // [tau] max number of c*tau ((A)Lambda)
+        Double_t								fCutV0ProtonNumSigmaMax;	// [sigmaTPC] max number of TPC sigma for proton PID (Lambda candidates)
         // members
         AliAODEvent*            fAOD;           //! input event
         AliPIDResponse*					fPIDResponse;		//! PID response
         AliAODTrack*						fTrack;					//! AOD track
-        Double_t 								fTrackPt;				//! track pT
-        Double_t 								fTrackPhi;				//! track phi
-        Double_t 								fTrackEta;				//! track eta
-        Int_t 									fNumV0s;					//! number of V0s in given event
+        Double_t 								fTrackPt;				// track pT
+        Double_t 								fTrackPhi;				// track phi
+        Double_t 								fTrackEta;				// track eta
+        Int_t 									fNumV0s;					// number of V0s in given event
         AliAODv0*			 					fV0;					//! V0 candidate
-        Bool_t 									fV0candK0s;		//! Is V0 a K0s candidate ?
-        Bool_t 									fV0candLambda;		//! Is V0 a Lambda or Anti-Lambda (ALambda) candidate ?
-        Double_t 								fV0MaxMassK0s;		//! Upper limit of K0s inv. mass window
-        Double_t 								fV0MinMassK0s;		//! Lower limit of K0s inv. mass window
-        Double_t 								fV0MaxMassLambda;		//! Upper limit of Lambda inv. mass window
-        Double_t 								fV0MinMassLambda;		//! Upper limit of Lambda inv. mass window
-        Short_t									fCentBinIndex;					//! event centrality bin index indicator
-        Float_t									fCentPercentile;					//! event centrality bin index indicator
-        Short_t                 fPtBinIndex;        //! track pT bin index indicator
-        Short_t									fMinvFlowBinIndex;		//! track pT bin index indicator
-        TComplex								fQvec2;					//! complex flow vector Q (n = 2)
-        TComplex								fQvec3;					//! complex flow vector Q (n = 3)
-        TComplex								fQvec4;					//! complex flow vector Q (n = 4)
-        TComplex								fQvec5;					//! complex flow vector Q (n = 5)
-        TComplex								fQvec2Gap00P;					//! complex flow vector Q (n = 2) with eta gap
-        TComplex								fQvec2Gap00N;					//! complex flow vector Q (n = 2) with eta gap
-        TComplex                fQvec2Gap04P;                   //! complex flow vector Q (n = 2) with eta gap
-        TComplex                fQvec2Gap04N;                   //! complex flow vector Q (n = 2) with eta gap
-        TComplex                fQvec2Gap08P;                   //! complex flow vector Q (n = 2) with eta gap
-        TComplex                fQvec2Gap08N;                   //! complex flow vector Q (n = 2) with eta gap
-        TComplex								fQvec2Gap09P;					//! complex flow vector Q (n = 2) with eta gap
-        TComplex								fQvec2Gap09N;					//! complex flow vector Q (n = 2) with eta gap
-        TComplex                fQvec2Gap10P;                   //! complex flow vector Q (n = 2) with eta gap
-        TComplex								fQvec2Gap10N;					//! complex flow vector Q (n = 2) with eta gap
-        TComplex								fPvec2[fNumPtBins];	//! complex vector p (n = 2) for pT-differential flow 
-        TComplex								fPvec2Gap00P[fNumPtBins];	//! complex vector p (n = 2) for pT-differential flow with eta gap
-        TComplex								fPvec2Gap04P[fNumPtBins];	//! complex vector p (n = 2) for pT-differential flow with eta gap
-        TComplex								fPvec2Gap08P[fNumPtBins];	//! complex vector p (n = 2) for pT-differential flow with eta gap
-        TComplex								fPvec2Gap10P[fNumPtBins];	//! complex vector p (n = 2) for pT-differential flow with eta gap
-        TComplex								fPvec3[fNumPtBins];	//! complex vector p (n = 2) for pT-differential flow 
+        Bool_t 									fV0candK0s;		// Is V0 a K0s candidate ?
+        Bool_t 									fV0candLambda;		// Is V0 a Lambda or Anti-Lambda (ALambda) candidate ?
+        Double_t 								fV0MaxMassK0s;		// Upper limit of K0s inv. mass window
+        Double_t 								fV0MinMassK0s;		// Lower limit of K0s inv. mass window
+        Double_t 								fV0MaxMassLambda;		// Upper limit of Lambda inv. mass window
+        Double_t 								fV0MinMassLambda;		// Upper limit of Lambda inv. mass window
+        Short_t									fCentBinIndex;					// event centrality bin index indicator
+        Float_t									fCentPercentile;					// event centrality bin index indicator
+        Short_t                 fPtBinIndex;        // track pT bin index indicator
+        Short_t									fMinvFlowBinIndex;		// track pT bin index indicator
+        TComplex								fQvec2;					// complex flow vector Q (n = 2)
+        TComplex								fQvec3;					// complex flow vector Q (n = 3)
+        TComplex								fQvec4;					// complex flow vector Q (n = 4)
+        TComplex								fQvec5;					// complex flow vector Q (n = 5)
+        TComplex								fQvec2Gap00P;				// complex flow vector Q (n = 2) with eta gap
+        TComplex								fQvec2Gap00N;					// complex flow vector Q (n = 2) with eta gap
+        TComplex                fQvec2Gap04P;                   // complex flow vector Q (n = 2) with eta gap
+        TComplex                fQvec2Gap04N;                   // complex flow vector Q (n = 2) with eta gap
+        TComplex                fQvec2Gap08P;                   // complex flow vector Q (n = 2) with eta gap
+        TComplex                fQvec2Gap08N;                   // complex flow vector Q (n = 2) with eta gap
+        TComplex								fQvec2Gap09P;					// complex flow vector Q (n = 2) with eta gap
+        TComplex								fQvec2Gap09N;					// complex flow vector Q (n = 2) with eta gap
+        TComplex                fQvec2Gap10P;                   // complex flow vector Q (n = 2) with eta gap
+        TComplex								fQvec2Gap10N;					// complex flow vector Q (n = 2) with eta gap
+        TComplex								fPvec2[fNumPtBins];	// complex vector p (n = 2) for pT-differential flow 
+        TComplex								fPvec2Gap00P[fNumPtBins];	// complex vector p (n = 2) for pT-differential flow with eta gap
+        TComplex								fPvec2Gap04P[fNumPtBins];	// complex vector p (n = 2) for pT-differential flow with eta gap
+        TComplex								fPvec2Gap08P[fNumPtBins];	// complex vector p (n = 2) for pT-differential flow with eta gap
+        TComplex								fPvec2Gap10P[fNumPtBins];	// complex vector p (n = 2) for pT-differential flow with eta gap
+        TComplex								fPvec3[fNumPtBins];	// complex vector p (n = 2) for pT-differential flow 
 
         TComplex                fVvec2Gap00P_K0s[fNumPtBins][fNumMinvFlowBinsK0s]; //
         TComplex                fVvec2Gap00N_K0s[fNumPtBins][fNumMinvFlowBinsK0s]; //
@@ -219,7 +219,7 @@ class AliAnalysisTaskFlowPID : public AliAnalysisTaskSE
         AliAnalysisTaskFlowPID(const AliAnalysisTaskFlowPID&); // not implemented
         AliAnalysisTaskFlowPID& operator=(const AliAnalysisTaskFlowPID&); // not implemented
 
-        ClassDef(AliAnalysisTaskFlowPID, 1);
+        ClassDef(AliAnalysisTaskFlowPID, 3);
 };
 
 #endif
