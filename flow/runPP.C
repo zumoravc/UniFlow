8ae7bb26c4974d65a327b3193f2a83a02ef2b464
Vojtech Pacik <vojtech.pacik@gcern.ch>
2016-12-13 10:48:49 +0100

    runPP: modification

--- flow/runPP.C
+++ flow/runPP.C
@@ -9,7 +9,7 @@ void runPP()
     Bool_t bMergeViaJDL = kTRUE;
     //Bool_t bMergeViaJDL = kFALSE;
 
-    TString sWorkDir = "pp-2016l-QA4";
+    TString sWorkDir = "pp-2016l-FB16";
     TString sOutDir = "outFlow";
     //TString sPeriod = "LHC16l";
     TString sPeriod = "LHC16l";
@@ -71,7 +71,7 @@ void runPP()
     
 
 
-    AliAnalysisTaskFlowPID* task1 = AddTaskFlowPID("flowPID_FB768_kINT7");
+    AliAnalysisTaskFlowPID* task1 = AddTaskFlowPID("flowPID_FB16_kINT7");
     //task1->SelectCollisionCandidates(AliVEvent::kINT7)
     // analysis cuts & switches
     task1->SetAODAnalysis(kTRUE);
@@ -93,9 +93,9 @@ void runPP()
     task1->SetTrackEtaMax(0.8);
     task1->SetTrackPtMin(0.2);
     task1->SetTrackPtMax(5.);
-    task1->SetTrackDCAzMax(0.5);
+    task1->SetTrackDCAzMax(0.0);
     task1->SetNumTPCclsMin(70);
-    task1->SetTrackFilterBit(768);
+    task1->SetTrackFilterBit(16);
     task1->SetUseBayesPID(kTRUE);
     task1->SetPIDBayesProbPionMin(0.8);
     task1->SetPIDBayesProbKaonMin(0.8);
@@ -126,13 +126,13 @@ void runPP()
     task1->SetV0sProtonNumSigmaMax(3.);
     task1->SetV0sProtonPIDPtMax(1.2); 
 
-
-    AliAnalysisTaskFlowPID* task2 = AddTaskFlowPID("flowPID_FB768_kHighMultV0");
+    
+    AliAnalysisTaskFlowPID* task2 = AddTaskFlowPID("flowPID_FB16_kHighMultV0");
     //task2->SelectCollisionCandidates(AliVEvent::kINT7)
     // analysis cuts & switches
     task2->SetAODAnalysis(kTRUE);
     task2->SetPPAnalysis(kTRUE);
-    task2->SetTrigger(1); // kINT7
+    task2->SetTrigger(1); 
     task2->SetSampling(kFALSE);
     //task2->SetUseOldCent(kFALSE);
     //task2->SetCentFlag(0);
@@ -149,9 +149,9 @@ void runPP()
     task2->SetTrackEtaMax(0.8);
     task2->SetTrackPtMin(0.2);
     task2->SetTrackPtMax(5.);
-    task2->SetTrackDCAzMax(0.5);
+    task2->SetTrackDCAzMax(0.);
     task2->SetNumTPCclsMin(70);
-    task2->SetTrackFilterBit(768);
+    task2->SetTrackFilterBit(16);
     task2->SetUseBayesPID(kTRUE);
     task2->SetPIDBayesProbPionMin(0.8);
     task2->SetPIDBayesProbKaonMin(0.8);
@@ -181,225 +181,6 @@ void runPP()
     task2->SetV0sLambdaNumTauMax(3.);
     task2->SetV0sProtonNumSigmaMax(3.);
     task2->SetV0sProtonPIDPtMax(1.2); 
-    
-    /*
-    AliAnalysisTaskFlowPID* task3 = AddTaskFlowPID("flowPID_FB1_kINT7");
-    //task3->SelectCollisionCandidates(AliVEvent::kINT7)
-    // analysis cuts & switches
-    task3->SetAODAnalysis(kTRUE);
-    task3->SetPPAnalysis(kTRUE);
-    task3->SetTrigger(0); // kINT7
-    task3->SetSampling(kFALSE);
-    //task3->SetUseOldCent(kFALSE);
-    //task3->SetCentFlag(0);
-    //task3->SetPeriod10h(kFALSE);
-    //task3->SetRejectPileUpSPD(kFALSE);
-    task3->SetDoFlowGenFramKatarina(kTRUE);
-    task3->SetDoOldFlow(kFALSE);
-    //task3->SetDoFlow(kTRUE);
-    task3->SetDiffFlow(kTRUE);
-    task3->SetPID(kTRUE);
-    task3->SetDoV0s(kTRUE);
-    // event selection
-    task3->SetPVtxZMax(10);
-    task3->SetTrackEtaMax(0.8);
-    task3->SetTrackPtMin(0.2);
-    task3->SetTrackPtMax(5.);
-    task3->SetNumTPCclsMin(70);
-    task3->SetTrackFilterBit(1);
-    task3->SetUseBayesPID(kTRUE);
-    task3->SetPIDBayesProbPionMin(0.8);
-    task3->SetPIDBayesProbKaonMin(0.8);
-    task3->SetPIDBayesProbProtonMin(0.8);
-    //task3->SetPionNumSigmasMax(3);
-    //task3->SetKaonNumSigmasMax(3);
-    //task3->SetProtonNumSigmasMax(3);
-    // V0 selection cuts
-    task3->SetV0sOnFly(kFALSE);
-    task3->SetV0sTPCRefit(kTRUE);
-    task3->SetV0sRejectKinks(kTRUE);
-    task3->SetV0sDCAPVMin(0.1);
-    task3->SetV0sDCAPVMax(0.);
-    task3->SetV0sDCADaughtersMax(1.);
-    task3->SetV0sDecayRadiusMin(5.);
-    task3->SetV0sDecayRadiusMax(0.);
-    task3->SetV0sDaughterPtMin(0.1);
-    task3->SetV0sDaughterEtaMax(0.8);
-    task3->SetV0sMotherEtaMax(0.8);
-    task3->SetV0sMotherRapMax(0.);
-    task3->SetV0sMotherPtMin(0.2);
-    task3->SetV0sMotherPtMax(10.);
-    task3->SetV0sK0sCPAMin(0.998);
-    task3->SetV0sLambdaCPAMin(0.998);
-    task3->SetV0sK0sNumTauMax(3.);
-    task3->SetV0sK0sArmenterosAlphaMin(0.2);
-    task3->SetV0sLambdaNumTauMax(3.);
-    task3->SetV0sProtonNumSigmaMax(3.);
-    task3->SetV0sProtonPIDPtMax(1.2);  
-
-    AliAnalysisTaskFlowPID* task4 = AddTaskFlowPID("flowPID_FB1_kHighMultV0");
-    //task4->SelectCollisionCandidates(AliVEvent::kINT7)
-    // analysis cuts & switches
-    task4->SetAODAnalysis(kTRUE);
-    task4->SetPPAnalysis(kTRUE);
-    task4->SetTrigger(1); // kINT7
-    task4->SetSampling(kFALSE);
-    //task4->SetUseOldCent(kFALSE);
-    //task4->SetCentFlag(0);
-    //task4->SetPeriod10h(kFALSE);
-    //task4->SetRejectPileUpSPD(kFALSE);
-    task4->SetDoFlowGenFramKatarina(kTRUE);
-    task4->SetDoOldFlow(kFALSE);
-    //task4->SetDoFlow(kTRUE);
-    task4->SetDiffFlow(kTRUE);
-    task4->SetPID(kTRUE);
-    task4->SetDoV0s(kTRUE);
-    // event selection
-    task4->SetPVtxZMax(10);
-    task4->SetTrackEtaMax(0.8);
-    task4->SetTrackPtMin(0.2);
-    task4->SetTrackPtMax(5.);
-    task4->SetNumTPCclsMin(70);
-    task4->SetTrackFilterBit(1);
-    task4->SetUseBayesPID(kTRUE);
-    task4->SetPIDBayesProbPionMin(0.8);
-    task4->SetPIDBayesProbKaonMin(0.8);
-    task4->SetPIDBayesProbProtonMin(0.8);
-    //task4->SetPionNumSigmasMax(3);
-    //task4->SetKaonNumSigmasMax(3);
-    //task4->SetProtonNumSigmasMax(3);
-    // V0 selection cuts
-    task4->SetV0sOnFly(kFALSE);
-    task4->SetV0sTPCRefit(kTRUE);
-    task4->SetV0sRejectKinks(kTRUE);
-    task4->SetV0sDCAPVMin(0.1);
-    task4->SetV0sDCAPVMax(0.);
-    task4->SetV0sDCADaughtersMax(1.);
-    task4->SetV0sDecayRadiusMin(5.);
-    task4->SetV0sDecayRadiusMax(0.);
-    task4->SetV0sDaughterPtMin(0.1);
-    task4->SetV0sDaughterEtaMax(0.8);
-    task4->SetV0sMotherEtaMax(0.8);
-    task4->SetV0sMotherRapMax(0.);
-    task4->SetV0sMotherPtMin(0.2);
-    task4->SetV0sMotherPtMax(10.);
-    task4->SetV0sK0sCPAMin(0.998);
-    task4->SetV0sLambdaCPAMin(0.998);
-    task4->SetV0sK0sNumTauMax(3.);
-    task4->SetV0sK0sArmenterosAlphaMin(0.2);
-    task4->SetV0sLambdaNumTauMax(3.);
-    task4->SetV0sProtonNumSigmaMax(3.);
-    task4->SetV0sProtonPIDPtMax(1.2); 
-    */
-    /*
-    AliAnalysisTaskFlowPID* task5 = AddTaskFlowPID("flowPID_FB128_kINT7");
-    //task5->SelectCollisionCandidates(AliVEvent::kINT7)
-    // analysis cuts & switches
-    task5->SetAODAnalysis(kTRUE);
-    task5->SetPPAnalysis(kTRUE);
-    task5->SetTrigger(0); // kINT7
-    task5->SetSampling(kFALSE);
-    //task5->SetUseOldCent(kFALSE);
-    //task5->SetCentFlag(0);
-    //task5->SetPeriod10h(kFALSE);
-    //task5->SetRejectPileUpSPD(kFALSE);
-    task5->SetDoFlowGenFramKatarina(kTRUE);
-    task5->SetDoOldFlow(kFALSE);
-    //task5->SetDoFlow(kTRUE);
-    task5->SetDiffFlow(kTRUE);
-    task5->SetPID(kTRUE);
-    task5->SetDoV0s(kTRUE);
-    // event selection
-    task5->SetPVtxZMax(10);
-    task5->SetTrackEtaMax(0.8);
-    task5->SetTrackPtMin(0.2);
-    task5->SetTrackPtMax(5.);
-    task5->SetNumTPCclsMin(70);
-    task5->SetTrackFilterBit(128);
-    task5->SetUseBayesPID(kTRUE);
-    task5->SetPIDBayesProbPionMin(0.8);
-    task5->SetPIDBayesProbKaonMin(0.8);
-    task5->SetPIDBayesProbProtonMin(0.8);
-    //task5->SetPionNumSigmasMax(3);
-    //task5->SetKaonNumSigmasMax(3);
-    //task5->SetProtonNumSigmasMax(3);
-    // V0 selection cuts
-    task5->SetV0sOnFly(kFALSE);
-    task5->SetV0sTPCRefit(kTRUE);
-    task5->SetV0sRejectKinks(kTRUE);
-    task5->SetV0sDCAPVMin(0.1);
-    task5->SetV0sDCAPVMax(0.);
-    task5->SetV0sDCADaughtersMax(1.);
-    task5->SetV0sDecayRadiusMin(5.);
-    task5->SetV0sDecayRadiusMax(0.);
-    task5->SetV0sDaughterPtMin(0.1);
-    task5->SetV0sDaughterEtaMax(0.8);
-    task5->SetV0sMotherEtaMax(0.8);
-    task5->SetV0sMotherRapMax(0.);
-    task5->SetV0sMotherPtMin(0.2);
-    task5->SetV0sMotherPtMax(10.);
-    task5->SetV0sK0sCPAMin(0.998);
-    task5->SetV0sLambdaCPAMin(0.998);
-    task5->SetV0sK0sNumTauMax(3.);
-    task5->SetV0sK0sArmenterosAlphaMin(0.2);
-    task5->SetV0sLambdaNumTauMax(3.);
-    task5->SetV0sProtonNumSigmaMax(3.);
-    task5->SetV0sProtonPIDPtMax(1.2);  
-
-    AliAnalysisTaskFlowPID* task6 = AddTaskFlowPID("flowPID_FB128_kHighMultV0");
-    //task6->SelectCollisionCandidates(AliVEvent::kINT7)
-    // analysis cuts & switches
-    task6->SetAODAnalysis(kTRUE);
-    task6->SetPPAnalysis(kTRUE);
-    task6->SetTrigger(1); // kINT7
-    task6->SetSampling(kFALSE);
-    //task6->SetUseOldCent(kFALSE);
-    //task6->SetCentFlag(0);
-    //task6->SetPeriod10h(kFALSE);
-    //task6->SetRejectPileUpSPD(kFALSE);
-    task6->SetDoFlowGenFramKatarina(kTRUE);
-    task6->SetDoOldFlow(kFALSE);
-    //task6->SetDoFlow(kTRUE);
-    task6->SetDiffFlow(kTRUE);
-    task6->SetPID(kTRUE);
-    task6->SetDoV0s(kTRUE);
-    // event selection
-    task6->SetPVtxZMax(10);
-    task6->SetTrackEtaMax(0.8);
-    task6->SetTrackPtMin(0.2);
-    task6->SetTrackPtMax(5.);
-    task6->SetNumTPCclsMin(70);
-    task6->SetTrackFilterBit(128);
-    task6->SetUseBayesPID(kTRUE);
-    task6->SetPIDBayesProbPionMin(0.8);
-    task6->SetPIDBayesProbKaonMin(0.8);
-    task6->SetPIDBayesProbProtonMin(0.8);
-    //task6->SetPionNumSigmasMax(3);
-    //task6->SetKaonNumSigmasMax(3);
-    //task6->SetProtonNumSigmasMax(3);
-    // V0 selection cuts
-    task6->SetV0sOnFly(kFALSE);
-    task6->SetV0sTPCRefit(kTRUE);
-    task6->SetV0sRejectKinks(kTRUE);
-    task6->SetV0sDCAPVMin(0.1);
-    task6->SetV0sDCAPVMax(0.);
-    task6->SetV0sDCADaughtersMax(1.);
-    task6->SetV0sDecayRadiusMin(5.);
-    task6->SetV0sDecayRadiusMax(0.);
-    task6->SetV0sDaughterPtMin(0.1);
-    task6->SetV0sDaughterEtaMax(0.8);
-    task6->SetV0sMotherEtaMax(0.8);
-    task6->SetV0sMotherRapMax(0.);
-    task6->SetV0sMotherPtMin(0.2);
-    task6->SetV0sMotherPtMax(10.);
-    task6->SetV0sK0sCPAMin(0.998);
-    task6->SetV0sLambdaCPAMin(0.998);
-    task6->SetV0sK0sNumTauMax(3.);
-    task6->SetV0sK0sArmenterosAlphaMin(0.2);
-    task6->SetV0sLambdaNumTauMax(3.);
-    task6->SetV0sProtonNumSigmaMax(3.);
-    task6->SetV0sProtonPIDPtMax(1.2); 
-    */
     
     if (!mgr->InitAnalysis()) return;
     mgr->SetDebugLevel(2);
@@ -444,7 +225,7 @@ void runPP()
 
         alienHandler->SetMasterResubmitThreshold(90);
         // number of files per subjob
-        alienHandler->SetSplitMaxInputFileNumber(100);
+        alienHandler->SetSplitMaxInputFileNumber(200);
         alienHandler->SetExecutable("FlowPID.sh");
         // specify how many seconds your job may take
         alienHandler->SetTTL(36000);
