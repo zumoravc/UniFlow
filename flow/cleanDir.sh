d9d9ff3a26348db6fdc7f41adf24983b9cd991bb
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-01-10 10:30:59 +0100

    cleanDir scripts added

---
+++ flow/cleanDir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Cleaning directory from task submission generated files 
+
+rm ./*.xml
+rm ./FlowPID*
+rm ./myAnalysis.C
+rm ./event_stat.root
+
+# compilation files
+rm ./*.d
+rm ./*.so
+
+
