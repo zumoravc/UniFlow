ebe6c76052cf4ac1fecc684a4451c4653c27aa40
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-02-20 00:17:16 +0100

    pPbRun2 8 TeV runlists added & generate modif'd

--- flow/scripts/generateRunPPbRun2.sh
+++ flow/scripts/generateRunPPbRun2.sh
@@ -61,10 +61,17 @@ void runPPbRun2()
     TString sPeriod = "${period}";
 
     // run switcher
-    // RunList_LHC16r_pass1_CentralBarrelTracking_hadronPID_20170202_v0.txt [12 runs]
+		// Run2 5.02 TeV
+		// RunList_LHC16r_pass1_CentralBarrelTracking_hadronPID_20170202_v0.txt [12 runs]
     Int_t runNumber[] = {266318, 266317, 266316, 266208, 266197, 266196, 266187, 265754, 265744, 265607, 265596, 265594};
     // RunList_LHC16s_pass1_CentralBarrelTracking_hadronPID_20170202_v0.txt [16 runs]
     // Int_t runNumber[] = {267110, 267081, 267077, 267072, 267070, 267030, 266998, 266997, 266994, 266993, 266944, 266886, 266885, 266883, 266882, 266437};
+
+		// Run2 8.16 TeV
+		// RunList_LHC16t_pass1_CentralBarrelTracking_hadronPID_20170202_v0.txt [4 runs]
+		// Int_t runNumber[] = {267166, 267165, 267164, 267163};
+		// RunList_LHC16q_pass1_CentralBarrelTracking_hadronPID_20170202_v0.txt [32 runs]
+		// Int_t runNumber[] = {265525, 265521, 265501, 265500, 265499, 265435, 265427, 265426, 265425, 265424, 265422, 265421, 265420, 265419, 265388, 265387, 265385, 265384, 265383, 265381, 265378, 265377, 265344, 265343, 265342, 265339, 265338, 265336, 265335, 265334, 265332, 265309};
 
     // since we will compile a class, tell root where to look for headers
     gROOT->ProcessLine(".include \$ROOTSYS/include");
@@ -118,7 +125,7 @@ void runPPbRun2()
     task1->SetAODAnalysis(kTRUE);
     task1->SetPPAnalysis(kTRUE);
     task1->SetTrigger(0); // kINT7
-    task1->SetSampling(kFALSE);
+    task1->SetSampling(kTRUE);
     //task1->SetUseOldCent(kFALSE);
     //task1->SetCentFlag(0);
     //task1->SetPeriod10h(kFALSE);
