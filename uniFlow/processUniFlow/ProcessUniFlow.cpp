de6ff85efc29eb09f021cec44f978e8844ace23d
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-04-21 15:56:23 +0200

    TestProjections() for testing if UniFlow task output is correct

--- uniFlow/processUniFlow/ProcessUniFlow.cpp
+++ uniFlow/processUniFlow/ProcessUniFlow.cpp
@@ -6,9 +6,16 @@
  */
 
 #include "TMath.h"
+#include "TString.h"
 #include "TFile.h"
 #include "TList.h"
-#include "TString.h"
+#include "TCanvas.h"
+#include "TProfile.h"
+#include "TProfile2D.h"
+#include "TProfile3D.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
 
 class ProcessUniFlow
 {
@@ -27,7 +34,7 @@ class ProcessUniFlow
 
   private:
     Bool_t      Initialize(); // initialization task
-
+    void        TestProjections(); // testing projection of reconstructed particles
     // printing output methods
     void        Fatal(TString sMsg, TString sMethod = ""); // printf the msg as error
     void        Error(TString sMsg, TString sMethod = ""); // printf the msg as error
@@ -72,6 +79,8 @@ void ProcessUniFlow::Run()
 
   Debug("Initialized");
 
+  TestProjections();
+
   return;
 }
 //_____________________________________________________________________________
@@ -102,6 +111,117 @@ Bool_t ProcessUniFlow::Initialize()
   fbInit = kTRUE;
   Info("Initialization succesfull","Initialize");
   return fbInit;
+}
+//_____________________________________________________________________________
+void ProcessUniFlow::TestProjections()
+{
+  Info("Testing profile projections");
+
+  ffInputFile->cd("UniFlow_CENT_wSDD");
+  // ffInputFile->ls();
+
+  TList* lFlow = (TList*) gDirectory->Get("Flow_UniFlow_CENT_wSDD");
+  // lFlow->ls();
+
+
+  TList* lRef = (TList*) lFlow->FindObject("fFlowRefs");
+  TProfile* Ref = (TProfile*) lRef->FindObject("fpRefs_<2>_harm2_gap-10_sample0");
+  if(!Ref) { Error("NotFound"); return; }
+  // Ref->Draw();
+  // NOTE: reference flow works
+
+  // projections charged flow
+  TList* lCharged = (TList*) lFlow->FindObject("fFlowCharged");
+  // lCharged->ls();
+  TProfile2D* p2Charged = (TProfile2D*) lCharged->FindObject("fp2Charged_<2>_harm2_gap-10_sample0");
+  if(!p2Charged) { Error("NotFound"); return; }
+  TProfile* p2ChargedProjY = p2Charged->ProfileY("p2ChargedProjY",10,10);
+
+  // TCanvas* cCharged = new TCanvas("cCharged","cCharged",1000,1000);
+  // cCharged->Divide(1,2);
+  // cCharged->cd(1);
+  // p2Charged->Draw("colz");
+  // cCharged->cd(2);
+  // p2ChargedProjY->Draw();
+
+  // projections V0s
+  TList* lV0s = (TList*) lFlow->FindObject("fFlowV0s");
+  if(!lV0s) { Error("NotFound"); return; }
+  lV0s->ls();
+
+  // entries
+  TH3D* h3K0sEntries = (TH3D*) lV0s->FindObject("fh3V0sEntriesK0s_gap-10")->Clone("h3K0sEntries");
+  if(!h3K0sEntries) { Error("NotFound"); return; }
+  TH1D* h3K0sEntriesProjX = h3K0sEntries->ProjectionX("h3K0sEntriesProjX"); // whole projeciton
+  TH1D* h3K0sEntriesProjY = h3K0sEntries->ProjectionY("h3K0sEntriesProjY"); // whole projeciton
+  TH1D* h3K0sEntriesProjZ = h3K0sEntries->ProjectionZ("h3K0sEntriesProjZ"); // whole projeciton
+  TH1D* h3K0sEntriesProjZsub = h3K0sEntries->ProjectionZ("h3K0sEntriesProjZsub",10,12,10,12); // projection in certain cent & pt range
+
+  TCanvas* cK0s = new TCanvas("cK0s","cK0s",1000,1000);
+  cK0s->Divide(2,3);
+  cK0s->cd(1);
+  h3K0sEntries->Draw();
+  cK0s->cd(2);
+  h3K0sEntriesProjZ->Draw();
+  cK0s->cd(3);
+  h3K0sEntriesProjX->Draw();
+  cK0s->cd(4);
+  h3K0sEntriesProjY->Draw();
+  cK0s->cd(5);
+  h3K0sEntriesProjZsub->Draw();
+  cK0s->cd(6);
+  // NOTE seems to work properly
+
+  // correlations
+  TProfile3D* p3K0sCor = (TProfile3D*) lV0s->FindObject("fp3V0sCorrK0s_<2>_harm2_gap-10")->Clone("p3K0sCor");
+  if(!p3K0sCor) { Error("NotFound"); return; }
+
+  TProfile2D* p3K0sCorProjZY = p3K0sCor->Project3DProfile("yz"); // projection over centrality
+  TProfile* p3K0sCorProjZ = p3K0sCorProjZY->ProfileX("p3K0sCorProjZ"); // projection over pT
+
+  TH3D* p3K0sCorHist = p3K0sCor->ProjectionXYZ("p3K0sCorHist"); // making TH3D from TProfile3D
+  p3K0sCorHist->Sumw2();
+  TProfile3D* p3K0sCorSub = (TProfile3D*) lV0s->FindObject("fp3V0sCorrK0s_<2>_harm2_gap-10")->Clone("p3K0sCorSub");
+  if(!p3K0sCorSub) { Error("NotFound"); return; }
+
+  // p3K0sCorSub->GetYaxis()->SetRangeUser(10,20);
+
+  // TProfile2D* p3K0sCorProjZYsub = p3K0sCorSub->Project3DProfile("xz"); // projection over centrality
+  // TProfile* p3K0sCorProjZsub = p3K0sCorProjZYsub->ProfileX("p3K0sCorProjZsub"); // projection over pT
+
+  // p3K0sCorHist->GetXaxis()->SetRange(10,20);
+
+  TProfile2D* p3K0sCorProjZYsub = p3K0sCorHist->Project3DProfile("yz"); // projection over centrality
+  TProfile* p3K0sCorProjZsub = p3K0sCorProjZYsub->ProfileX("p3K0sCorProjZsub"); // projection over pT
+
+
+  Int_t binx = 10, biny = 50, binz = 1;
+  printf("Profile: %f ± %f | ", p3K0sCor->GetBinContent(binx,biny,binz), p3K0sCor->GetBinError(binx,biny,binz));
+  printf("TH: %f ± %f\n", p3K0sCorHist->GetBinContent(binx,biny,binz), p3K0sCorHist->GetBinError(binx,biny,binz));
+
+  printf("Projected 2D from Profile: %f ± %f | ", p3K0sCorProjZY->GetBinContent(1,5), p3K0sCorProjZY->GetBinError(1,5));
+  printf(" from TH: %f ± %f\n", p3K0sCorProjZYsub->GetBinContent(1,5), p3K0sCorProjZYsub->GetBinError(1,5));
+
+  TCanvas* cK0sCor = new TCanvas("cK0sCor","cK0sCor",1000,1000);
+  cK0sCor->Divide(2,3);
+  cK0sCor->cd(1);
+  p3K0sCor->Draw();
+  cK0sCor->cd(2);
+  // p3K0sCorHist->Draw();
+  p3K0sCorSub->Draw();
+  cK0sCor->cd(3);
+  p3K0sCorProjZY->Draw("colz");
+  cK0sCor->cd(4);
+  p3K0sCorProjZYsub->Draw("colz");
+  cK0sCor->cd(5);
+  p3K0sCorProjZ->Draw();
+  cK0sCor->cd(6);
+  p3K0sCorProjZsub->Draw();
+
+
+
+
+  return;
 }
 //_____________________________________________________________________________
 void ProcessUniFlow::Fatal(TString sMsg, TString sMethod)
