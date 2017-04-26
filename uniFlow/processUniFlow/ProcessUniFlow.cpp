3b93e8070793a21b36df2f814c20e885d38e374f
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-04-26 02:43:53 +0200

    ExtractK0s copied -> ExtractPhi; cosmetics

--- uniFlow/processUniFlow/ProcessUniFlow.cpp
+++ uniFlow/processUniFlow/ProcessUniFlow.cpp
@@ -1038,34 +1038,168 @@ TH2D* ProcessUniFlow::DoProject2D(TH3D* h3, const char * name, const char * titl
 //_____________________________________________________________________________
 Bool_t ProcessUniFlow::ExtractFlowPhi(TH1* hInvMass, TH1* hInvMassBG, TH1* hFlowMass, Double_t &dFlow, Double_t &dFlowError, TCanvas* canFitInvMass)
 {
+  if(!hInvMass) { Error("InvMass (signal) histogram does not exists!","ExtractFlowPhi"); return kFALSE; }
+  if(!hInvMassBG) { Error("InvMass (BG) histogram does not exists!","ExtractFlowPhi"); return kFALSE; }
+	if(!hFlowMass) { Error("Flow-mass histogram does not exists!","ExtractFlowPhi"); return kFALSE;	}
+	if(!canFitInvMass) { Error("Canvas not found!","ExtractFlowPhi"); return kFALSE; }
+
+	// Reseting the canvas (removing drawn things)
+	canFitInvMass->Clear();
+
+	TString sOutputFormat = fsOutputFileFormat;
+	const Short_t iNumSigmas = 7;
+	Double_t dMeanShot = 0;
+	Double_t dSigmaShot = 0;
+	Double_t dMassLow = 0;
+	Double_t dMassHigh = 0;
+
   return kFALSE;
+
+	//TCanvas* canFitInvMass = new TCanvas("canFitInvMass","FitInvMass",1200,1200);
+	canFitInvMass->Divide(3,2);
+
+	TH1D* hInvMass_side = 0x0;
+	TH1D* hInvMass_residual = 0x0;
+	TH1D* hInvMass_ratio = 0x0;
+
+	TH1D* hFlowMass_side = 0x0;
+
+	const Short_t iNumBinsMassFlow = hFlowMass->GetNbinsX();
+
+	// inv mass fitts
+	TF1* fitShot = 0x0;
+	TF1* fitSide = 0x0;
+	TF1* fitRatio = 0x0;
+
+	// flow mass fits
+	TF1* fitFlowSide = 0x0;
+	TF1* fitFlowTot = 0x0;
+
+
+	hInvMass_side = (TH1D*) hInvMass->Clone("hInvMass_side");
+	hInvMass_residual = (TH1D*) hInvMass->Clone("hInvMass_residual");
+
+
+	printf("\n====== K0s: Fitting InvMass first shot ========\n");
+	canFitInvMass->cd(1);
+	fitShot = new TF1("fitShot","gaus(0)+pol2(3)",0.4,0.6);
+	fitShot->SetNpx(10000);
+	fitShot->SetParameter(1,0.5);
+	fitShot->SetParLimits(1,0.485,0.515);
+	fitShot->SetParameter(2,0.01);
+	fitShot->SetParLimits(2,0.,0.05);
+	hInvMass->Fit("fitShot","R");
+
+	// TODO checking the fitting results
+
+	// extract mean & sigma for sidebands fitting reagion
+	dMeanShot = fitShot->GetParameter(1);
+	dSigmaShot = fitShot->GetParameter(2);
+	dMassLow = dMeanShot - iNumSigmas*dSigmaShot;
+	dMassHigh = dMeanShot + iNumSigmas*dSigmaShot;
+	printf("=========================\nFitting region: %f - %f \n==========================\n", dMassLow,dMassHigh);
+
+	const Short_t iNumBinsMass = hInvMass->GetNbinsX();
+	for(Short_t iMass(1); iMass < iNumBinsMass+1; iMass++)
+	{
+		// Excluding mass peak window (setting errors to inf)
+		if(hInvMass_side->GetBinCenter(iMass) > dMassLow && hInvMass_side->GetBinCenter(iMass) < dMassHigh)
+		{
+			hInvMass_side->SetBinError(iMass,9999999999999);
+		}
+	}
+	canFitInvMass->cd(2);
+	hInvMass_side->SetMaximum(hInvMass->GetMaximum()); // setting maximum & minimum (otherwise overshoteed with errors)
+	hInvMass_side->SetMinimum(0);
+	hInvMass_side->Draw();
+
+	// fitting background in sidebands
+	printf("\n====== K0s: Fitting InvMass side bands ========\n");
+	fitSide = new TF1("fitSide","pol2(0)",0.4,0.6);
+	fitSide->SetNpx(10000);
+	hInvMass_side->Fit("fitSide","R");
+
+	Double_t dContent = 0;
+	for(Short_t iMass(1); iMass < iNumBinsMass+1; iMass++)
+	{
+		dContent = hInvMass_side->GetBinContent(iMass) - fitSide->Eval(hInvMass_side->GetBinCenter(iMass));
+		hInvMass_residual->SetBinContent(iMass,dContent);
+	}
+
+	canFitInvMass->cd(3);
+	hInvMass_residual->Draw();
+
+	hInvMass_ratio = (TH1D*) hInvMass_residual->Clone("hInvMass_ratio");
+	hInvMass_ratio->Sumw2();
+	hInvMass_ratio->Divide(hInvMass);
+
+	printf("\n====== K0s: Fitting InvMass sig/tot ratio ========\n");
+	canFitInvMass->cd(4);
+
+	//hInvMass_ratio->Draw();
+	fitRatio = new TF1("fitRatio","gaus(0)+pol3(3)",0.4,0.6);
+	fitRatio->SetNpx(1000);
+	fitRatio->SetParameter(0,0.98);
+	fitRatio->SetParameter(1,0.5);
+	fitRatio->SetParLimits(1,0.48,0.51);
+	fitRatio->SetParameter(2,0.01);
+	fitRatio->SetParLimits(2,0.,0.05);
+	hInvMass_ratio->Fit("fitRatio","R");
+
+	// flow mass Fitting
+	hFlowMass_side = (TH1D*) hFlowMass->Clone("hFlowMass_side");
+	hFlowMass_side->SetMaximum(1.5*hFlowMass->GetMaximum());
+	hFlowMass_side->SetMinimum(0.5*hFlowMass->GetMinimum());
+
+	// fitting side bands
+	for(Short_t iMass(1); iMass < iNumBinsMassFlow+1; iMass++)
+	{
+		// Excluding mass peak window (setting errors to inf)
+		if(hFlowMass_side->GetBinCenter(iMass) > dMassLow && hFlowMass_side->GetBinCenter(iMass) < dMassHigh)
+		{
+			hFlowMass_side->SetBinError(iMass,9999999999999);
+		}
+	}
+
+	printf("\n====== K0s: Fitting FlowMass sidebands ========\n");
+	canFitInvMass->cd(6);
+	fitFlowSide = new TF1("fitFlowSide","pol2(0)",0.4,0.6);
+	hFlowMass_side->Fit("fitFlowSide","R");
+
+	canFitInvMass->cd(5);
+	printf("\n====== K0s: Fitting FlowMass total flow ========\n");
+	fitFlowTot = new TF1("fitFlowTot","[0]*(gaus(1)+pol3(4)) + ( 1-(gaus(1)+pol3(4)) )*pol2(8)",0.4,0.6);
+	// Inv mass ratio signal/total
+	fitFlowTot->FixParameter(1,fitRatio->GetParameter(0));
+	fitFlowTot->FixParameter(2,fitRatio->GetParameter(1));
+	fitFlowTot->FixParameter(3,fitRatio->GetParameter(2));
+	fitFlowTot->FixParameter(4,fitRatio->GetParameter(3));
+	fitFlowTot->FixParameter(5,fitRatio->GetParameter(4));
+	fitFlowTot->FixParameter(6,fitRatio->GetParameter(5));
+	fitFlowTot->FixParameter(7,fitRatio->GetParameter(6));
+	// FlowMass backround / sidebands
+	fitFlowTot->FixParameter(8,fitFlowSide->GetParameter(0));
+	fitFlowTot->FixParameter(9,fitFlowSide->GetParameter(1));
+	fitFlowTot->FixParameter(10,fitFlowSide->GetParameter(2));
+	hFlowMass->Fit("fitFlowTot","R");
+
+	dFlow = fitFlowTot->GetParameter(0);
+	dFlowError = fitFlowTot->GetParError(0);
+
+  return kTRUE;
 }
 //_____________________________________________________________________________
 Bool_t ProcessUniFlow::ExtractFlowK0s(TH1* hInvMass, TH1* hFlowMass, Double_t &dFlow, Double_t &dFlowError, TCanvas* canFitInvMass)
 {
-	if(!hInvMass)
-	{
-		Error("ExtractFlowK0s","Inv. Mass histogram does not exists!");
-		return kFALSE;
-	}
-
-	if(!hFlowMass)
-	{
-		Error("ExtractFlowK0s","Flow Mass histogram does not exists!");
-		return kFALSE;
-	}
-
-	if(!canFitInvMass)
-	{
-		Error("ExtractFlowK0s","Canvas not found!");
-		return kFALSE;
-	}
+	if(!hInvMass) { Error("Inv. Mass histogram does not exists!","ExtractFlowK0s"); return kFALSE; }
+	if(!hFlowMass) { Error("Flow Mass histogram does not exists!","ExtractFlowK0s"); return kFALSE; }
+	if(!canFitInvMass) { Error("Canvas not found!","ExtractFlowK0s"); return kFALSE; }
 
 	// Reseting the canvas (removing drawn things)
 	canFitInvMass->Clear();
 
 	// Fitting K0s
-	const TString sOutputFormat = "pdf";
+	const TString sOutputFormat = fsOutputFileFormat;
 	const Short_t iNumSigmas = 7;
 	Double_t dMeanShot = 0;
 	Double_t dSigmaShot = 0;
@@ -1207,29 +1341,15 @@ Bool_t ProcessUniFlow::ExtractFlowK0s(TH1* hInvMass, TH1* hFlowMass, Double_t &d
 //_____________________________________________________________________________
 Bool_t ProcessUniFlow::ExtractFlowLambda(TH1* hInvMass, TH1* hFlowMass, Double_t &dFlow, Double_t &dFlowError, TCanvas* canFitInvMass)
 {
-	if(!hInvMass)
-	{
-		Error("ExtractFlowLambda","Inv. Mass histogram does not exists!");
-		return kFALSE;
-	}
-
-	if(!hFlowMass)
-	{
-		Error("ExtractFlowLambda","Flow Mass histogram does not exists!");
-		return kFALSE;
-	}
-
-	if(!canFitInvMass)
-	{
-		Error("ExtractFlowLambda","Canvas not found!");
-		return kFALSE;
-	}
+	if(!hInvMass) { Error("Inv. Mass histogram does not exists!","ExtractFlowLambda"); return kFALSE; }
+	if(!hFlowMass) { Error("Flow Mass histogram does not exists!","ExtractFlowLambda");	return kFALSE; }
+	if(!canFitInvMass) { Error("Canvas not found!","ExtractFlowLambda"); return kFALSE; }
 
 	// Reseting the canvas (removing drawn things)
 	canFitInvMass->Clear();
 
 	// Fitting K0s
-	const TString sOutputFormat = "pdf";
+	const TString sOutputFormat = fsOutputFileFormat;
 	const Short_t iNumSigmas = 6;
 	const Double_t fitLimitLow = 1.095;
 	const Double_t fitLimitHigh = 1.15;
