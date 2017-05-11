34032f26348234621aff2e8ebe7b999e01c07ea6
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-05-11 22:17:43 +0200

    Run macro prepared for Refs processing & desampling developement

--- uniFlow/processUniFlow/RunProcessUniFlow.C
+++ uniFlow/processUniFlow/RunProcessUniFlow.C
@@ -6,7 +6,7 @@
  * Author: Vojtech Pacik (vojtech.pacik@cern.ch), NBI, 2017
  */
 
-void RunProcessUniFlow(const char* sOutputFilePath)
+void RunProcessUniFlow(const char* sOutputFilePath = "")
 {
 	gSystem->AddIncludePath("-I/Users/vpacik/NBI/Flow/uniFlow/processUniFlow/");
 	gROOT->SetMacroPath(Form("%s:/Users/vpacik/NBI/Flow/uniFlow/processUniFlow/",gROOT->GetMacroPath()));
@@ -14,41 +14,42 @@ void RunProcessUniFlow(const char* sOutputFilePath)
 
 	ProcessUniFlow* process = new ProcessUniFlow();
 
-	process->SetInputFilePath("/Users/vpacik/NBI/Flow/results/uniFlow_ver4_V0A/");
-	process->SetInputFileName("AnalysisResults_CENTwSDD_16q.root");
-	process->SetTaskName("UniFlow_V0A");
-	// process->SetInputFilePath("/Users/vpacik/NBI/Flow/uniFlow/processUniFlow/test");
-	// process->SetInputFileName("AnalysisResults.root");
-	// process->SetTaskName("UniFlow");
-	process->SetOutputFilePath(sOutputFilePath);
-	// process->SetOutputFilePath("/Users/vpacik/NBI/Flow/uniFlow/processUniFlow/taskVer2");
-	// process->SetDebug();
+	// process->SetInputFilePath("/Users/vpacik/NBI/Flow/results/uniFlow_ver4_V0A/");
+	// process->SetInputFileName("AnalysisResults_CENTwSDD_16q.root");
+	// process->SetTaskName("UniFlow_V0A");
+	process->SetInputFilePath("/Users/vpacik/NBI/Flow/uniFlow/processUniFlow/test");
+	process->SetInputFileName("AnalysisResults.root");
+	process->SetTaskName("UniFlow");
+	// process->SetOutputFilePath(sOutputFilePath);
+	process->SetOutputFilePath("/Users/vpacik/NBI/Flow/uniFlow/processUniFlow/testRefs");
+	process->SetOutputFileName("UniFlow_test.root");
+	process->SetDebug();
 	// process->SuggestMultBinning(4);
 
 	// setting multiplicity binning
 	// Double_t dMultBinning[] = {0,30,45,75,180};
-	Double_t dMultBinning[] = {0,5,10,20,30,40,50,60,70,80};
+	Double_t dMultBinning[] = {0,5,10,20,30,40,50,60,70,80,90,100};
 	Int_t iSizeMult = sizeof(dMultBinning)/sizeof(dMultBinning[0]);
 	process->SetMultiplicityBins(dMultBinning,iSizeMult);
 
-	// FlowTask* task1 = new FlowTask("testing",FlowTask::kRefs);
-	// task1->SetHarmonics(2);
-	// task1->SetEtaGap(-1.);
-	// process->AddTask(task1);
+	FlowTask* task1 = new FlowTask("Refs",FlowTask::kRefs);
+	task1->SetHarmonics(2);
+	task1->SetEtaGap(0.8);
+	process->AddTask(task1);
 
 	// Double_t dPtBinning[] = {0.5,1.};
-	Double_t dPtBinning[] = {0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.};
-	Int_t iSizePt = sizeof(dPtBinning)/sizeof(dPtBinning[0]);
+	// Double_t dPtBinning[] = {0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0,2.2,2.4,2.6,2.8,3.};
+	// Int_t iSizePt = sizeof(dPtBinning)/sizeof(dPtBinning[0]);
 
-	FlowTask* task7 = new FlowTask("K0s",FlowTask::kK0s);
-	task7->SetHarmonics(2);
-	task7->SetEtaGap(0.8);
-	task7->SetInvMassRebin(2);
-	task7->SetFlowMassRebin(2);
-	// task7->SetShowMultDist(kTRUE);
-	task7->SetPtBins(dPtBinning,iSizePt);
-	// task7->SuggestPtBinning(1,30000);
-	process->AddTask(task7);
+	// FlowTask* task7 = new FlowTask("K0s",FlowTask::kK0s);
+	// task7->SetHarmonics(2);
+	// task7->SetEtaGap(0.8);
+	// task7->SetInvMassRebin(2);
+	// task7->SetFlowMassRebin(2);
+	// // task7->SetShowMultDist(kTRUE);
+	// task7->SetPtBins(dPtBinning,iSizePt);
+	// // task7->SuggestPtBinning(1,30000);
+	// process->AddTask(task7);
 	//
 
 	// FlowTask* task8 = new FlowTask("K0s",FlowTask::kK0s);
@@ -86,8 +87,8 @@ void RunProcessUniFlow(const char* sOutputFilePath)
 	//
 
 	// Double_t dPtBinningPhi[] = {0.5,0.8,1.1,1.4,1.7,2.,2.3,2.6,2.9,3.2,3.5,4,4.5,5.};
-	Double_t dPtBinningPhi[] = {1.,1.5,2.05};
-	Int_t iSizePtPhi = sizeof(dPtBinningPhi)/sizeof(dPtBinningPhi[0]);
+	// Double_t dPtBinningPhi[] = {1.,1.5,2.05};
+	// Int_t iSizePtPhi = sizeof(dPtBinningPhi)/sizeof(dPtBinningPhi[0]);
 
 
 	// FlowTask* task2 = new FlowTask("Phi",FlowTask::kPhi);
