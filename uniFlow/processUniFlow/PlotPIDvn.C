cffce39405d78dc3a4a8453e666a4707150eed2a
Vojtech Pacik <vojtech.pacik@gcern.ch>
2017-05-15 09:49:56 +0200

    Marker setting added; plotting option modified

--- uniFlow/processUniFlow/PlotPIDvn.C
+++ uniFlow/processUniFlow/PlotPIDvn.C
@@ -11,6 +11,7 @@
 #include "TFile.h"
 #include "TList.h"
 #include "TCanvas.h"
+#include "TLegend.h"
 #include "TLine.h"
 #include "TProfile.h"
 #include "TProfile2D.h"
@@ -23,12 +24,18 @@
 void PlotPIDvn()
 {
   TString sInputFile = TString("/Users/vpacik/NBI/Flow/uniFlow/processUniFlow/testRefs/UniFlow_PID.root");
+
   // colors setting
   Color_t colCharged = kBlack;
   Color_t colPion = kRed;
   Color_t colKaon = kBlue;
   Color_t colProton = kGreen+2;
 
+  // markers setting
+  Int_t markCharged = 20;
+  Int_t markPion = 20;
+  Int_t markKaon = 20;
+  Int_t markProton = 20;
 
   // openning input file
   TFile* fInputFile = new TFile(sInputFile.Data(),"READ");
@@ -49,21 +56,35 @@ void PlotPIDvn()
   hFlowCharged->SetStats(kFALSE);
   hFlowCharged->SetLineColor(colCharged);
   hFlowCharged->SetMarkerColor(colCharged);
+  hFlowCharged->SetMarkerStyle(markCharged);
 
   hFlowPion->SetLineColor(colPion);
   hFlowPion->SetMarkerColor(colPion);
+  hFlowPion->SetMarkerStyle(markPion);
 
   hFlowKaon->SetLineColor(colKaon);
   hFlowKaon->SetMarkerColor(colKaon);
+  hFlowKaon->SetMarkerStyle(markKaon);
 
   hFlowProton->SetLineColor(colProton);
   hFlowProton->SetMarkerColor(colProton);
+  hFlowProton->SetMarkerStyle(markProton);
+
+  // making legend
+  TLegend* legend = new TLegend(0.19,0.7,0.5,0.82);
+  legend->SetBorderSize(0);
+  legend->AddEntry(hFlowCharged,"h^{#pm}","pel");
+  legend->AddEntry(hFlowPion,"#pi","pel");
+  legend->AddEntry(hFlowKaon,"K","pel");
+  legend->AddEntry(hFlowProton,"p","pel");
+
 
   // drawing stuff
-  hFlowCharged->Draw();
-  hFlowPion->Draw("same");
-  hFlowKaon->Draw("same");
-  hFlowProton->Draw("same");
+  hFlowCharged->Draw("hist p e1 x0");
+  hFlowPion->Draw("hist p e1 x0 same");
+  hFlowKaon->Draw("hist p e1 x0  same");
+  hFlowProton->Draw("hist p e1 x0 same");
+  legend->Draw();
 
   return;
 }
