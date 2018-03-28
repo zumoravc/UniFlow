ac86b5bce5827871625546b8f85c70b341e23c2c
Vojtech Pacik <vojtech.pacik@gcern.ch>
2018-03-28 13:09:02 +0200

    ViewWeights.C macro improved (mainly plots)

--- uniFlow/macros/TrackWeightsGF/ViewWeights.C
+++ uniFlow/macros/TrackWeightsGF/ViewWeights.C
@@ -1,40 +1,70 @@
+// =================================================================================================
+// ViewWeights.C
+// Author: Vojtech Pacik (vojtech.pacik@cern.ch), NBI, 2016-2018
+//
+// Macro created for checking NUA (phi,eta) 2D-weights for UniFlow output
+// =================================================================================================
+
 #include "TFile.h"
 #include "TH1.h"
 #include "TH2.h"
 #include "TH3.h"
 #include "TCanvas.h"
 
-
 TH2D* LoadWeights(const TH3D* weights);
-
 
 void ViewWeights()
 {
-  TString sPath = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/pp-16l";
+  Bool_t bSavePlots = kTRUE;
+  TString sWeightName = "fh3AfterWeights";
+  // TString sWeightName = "fh3Weights";
+  // TString sPath = "./hadd_test/";
+
+  // TString sPath = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/syst/tracking/merged-16q-nua/";
+  // TString sPath = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/syst/tracking/pPb-16q-FAST-nua/";
+  // TString sPath = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/pPb-16q-woSDD-nua/";
+  // TString sPath = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/pPb-16q-FAST-nua/";
+  TString sPath = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/merged-pPb-16qt-nua/";
+  TString sFileName = "AnalysisResults.root";
   TString sTaskTag = "UniFlow";
-  TString species[] = {"Phi"};
+  // TString sTaskTag = "UniFlow_cls";
+  TString species[] = {"Refs","Charged","Pion","Kaon","Proton","K0s","Lambda","Phi"};
+  // TString species[] = {"K0s"};
+
+ // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
   Int_t iNumPart = sizeof(species)/sizeof(species[0]);
 
-
-
-  TFile* fInput = TFile::Open(Form("%s/AnalysisResults_intermerge.root",sPath.Data()),"READ");
+  TFile* fInput = TFile::Open(Form("%s%s",sPath.Data(),sFileName.Data()),"READ");
   fInput->cd(sTaskTag.Data());
   TList* list =  (TList*) gDirectory->Get(Form("Flow_Weights_%s",sTaskTag.Data()));
+
+  TCanvas* can = new TCanvas("can","can",iNumPart*400,400);
+  can->Divide(iNumPart,2);
 
   for(Short_t part = 0; part < iNumPart; part++)
   {
     printf(" -part %d (out of %d)\n",part+1,iNumPart);
-    // h3Weights = (TH3D*) listTemp->FindObject(Form("fh3Weights%s",species[part].Data()));
-    TH3D* h3Weights = (TH3D*) list->FindObject(Form("fh3Weights%s",species[part].Data()));
-    if(!h3Weights) { printf("Hist 'fh3Weights%s' not found\n",species[part].Data()); continue; }
+    TH3D* h3Weights = (TH3D*) list->FindObject(Form("%s%s",sWeightName.Data(),species[part].Data()));
+    if(!h3Weights) { printf("Hist '%s%s' not found\n",sWeightName.Data(),species[part].Data()); return; }
+
     TH2D* h2Weights = LoadWeights(h3Weights);
-    // CheckWeights(h3Weights, iRunList[iRun]);
+    if(!h2Weights) { printf("Hist h2Weights '%s%s' not found\n",sWeightName.Data(),species[part].Data()); return; }
     h2Weights->SetName(Form("%s",species[part].Data()));
     h2Weights->SetTitle(Form("%s",species[part].Data()));
-    // listRun->Add(h2Weights);
 
+    TH1D* hWeights = (TH1D*) h2Weights->ProjectionX();
+    can->cd(part+1);
+    // gPad->SetLogz();
+    // h2Weights->SetStats(0);
     h2Weights->Draw("colz");
+    can->cd(part+1+iNumPart);
+    // hWeights->SetMinimum(0.0);
+    // hWeights->SetStats(0);
+    hWeights->Draw("colz");
   }
+
+  if(bSavePlots) { can->SaveAs(Form("%s/%s_%s.pdf",sPath.Data(),sWeightName.Data(), sTaskTag.Data())); }
 
 }
 
@@ -42,7 +72,7 @@ TH2D* LoadWeights(const TH3D* weights)
 {
   if(!weights) { printf("PrepareWeights: no input histo\n"); return 0x0;}
 
-  TH2D* weights_2d = (TH2D*) weights->Project3D("xy");
+  TH2D* weights_2d = (TH2D*) weights->Project3D("yx");
   return weights_2d;
 }
 //_____________________________________________________________________________
