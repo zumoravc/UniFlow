945ff7f1e1196e2d0a62e65fecb9e57efdf92d8c
Vojtech Pacik <vojtech.pacik@gcern.ch>
2018-04-02 22:46:48 +0200

    Macros for systematics added to /macros

---
+++ uniFlow/macros/systematics_qm18/EvaluateSyst.C
@@ -0,0 +1,239 @@
+#include "TFile.h"
+#include "TH1.h"
+#include "TList.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TLine.h"
+#include "TLegend.h"
+#include "TMath.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include "TColor.h"
+
+void StyleHist(TH1* hist, Color_t color = kRed, Style_t markerStyle = kOpenCircle, Bool_t showStats = kFALSE);
+void SetCustomPalette();
+TH1D* ProcessList(TList* listDiff, TList* listBarlow, Double_t dCutBarlow);
+
+Double_t dCutBarlow = 1.0;
+
+TString sTopInputPath = "./out/";
+TString sOutPath = "./out/figures_wBarlowCheck/";
+// TString sOutPath = "./out/figures_noFB/";
+// TString sOutPath = "./out/figures_wBarlowCheck_noFB/";
+
+// TString sSpecies[] = {"Charged"}; TString sFiles[] = {"tracking/cls", "tracking/PV"};
+// TString sSpecies[] = {"Pion","Kaon","Proton","Phi"}; TString sFiles[] = {"tracking/cls", "tracking/PV", "pid/3sigma", "pid/bayes90"};
+// TString sSpecies[] = {"K0s","Lambda"}; TString sFiles[] = {"tracking/cls", "tracking/PV","v0s/3sigma", "v0s/CPA", "v0s/decayRad", "v0s/DCAdaughters"};
+
+// TString sSpecies[] = {"Charged"}; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB"};
+// TString sSpecies[] = {"Pion","Kaon","Proton","Phi"}; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB", "pid/3sigma", "pid/bayes90"};
+TString sSpecies[] = {"K0s","Lambda"}; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB","v0s/3sigma", "v0s/CPA", "v0s/decayRad", "v0s/DCAdaughters"};
+
+// // TString sSpecies[] = {"Charged"}; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB"};
+// // TString sSpecies[] = {"Pion","Kaon","Proton","Phi"}; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB", "pid/3sigma", "pid/bayes90"};
+// TString sSpecies = "Kaon"; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB", "pid/3sigma", "pid/bayes90"};
+// // TString sSpecies = "Proton"; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB", "pid/3sigma", "pid/bayes90"};
+// // TString sSpecies = "K0s"; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB","v0s/3sigma", "v0s/CPA", "v0s/decayRad", "v0s/DCAdaughters"};
+// // TString sSpecies = "Lambda"; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB","v0s/3sigma", "v0s/CPA", "v0s/decayRad", "v0s/DCAdaughters"};
+// // TString sSpecies = "Phi"; TString sFiles[] = {"tracking/cls", "tracking/PV", "tracking/FB", "pid/3sigma", "pid/bayes90"};
+Int_t iNumFiles = sizeof(sFiles)/sizeof(sFiles[0]);
+Int_t iNumSpecies = sizeof(sSpecies)/sizeof(sSpecies[0]);
+
+TString sCentLabel[] = {"0-10%","10-20%","20-40%","40-60%"};
+Int_t iNumCent = sizeof(sCentLabel)/sizeof(sCentLabel[0]);
+
+Color_t colors[] = {kBlack,kRed, kBlue, kGreen+2, kViolet-1, kOrange+1, kMagenta-1, kOrange-4};
+
+void EvaluateSyst()
+{
+  // SetCustomPalette();
+
+  gSystem->mkdir(sOutPath.Data(),kTRUE);
+
+
+  for(Int_t iSpec(0); iSpec < iNumSpecies; ++iSpec)
+  {
+
+    TFile* fileOut = TFile::Open(Form("%s/syst_%s.root",sOutPath.Data(),sSpecies[iSpec].Data()),"RECREATE");
+    if(!fileOut) { printf("No fileOut \n"); return;}
+
+    for(Int_t iCent(0); iCent < iNumCent; ++iCent)
+    {
+
+      TList* listDiff = new TList();
+      TList* listBarlow = new TList();
+
+      TLegend* leg = new TLegend(0.3,0.3,0.7,0.7);
+      leg->SetBorderSize(0);
+      leg->SetFillColorAlpha(0,0);
+      leg->SetHeader(Form("%s (%s)",sSpecies[iSpec].Data(),sCentLabel[iCent].Data()));
+
+      for(Int_t iFile(0); iFile < iNumFiles; ++iFile)
+      {
+        TString sInputFile = sFiles[iFile];
+        TFile* fileInput = TFile::Open(Form("%s%s.root",sTopInputPath.Data(),sInputFile.Data()),"READ");
+        if(!fileInput) { printf("ERROR : fileInput not found\n"); return; }
+        // fileInput->ls();
+
+        TList* list = (TList*) fileInput->Get(Form("%s_cent%d",sSpecies[iSpec].Data(),iCent));
+        if(!list) { printf("ERROR : list not found\n"); return; }
+
+        // list->ls();
+
+        TH1D* histBarlow = (TH1D*) list->FindObject("histBarlow");
+        if(!histBarlow) { printf("ERROR : histBarlow not found\n"); return; }
+        TH1D* histDiff = (TH1D*) list->FindObject("histDiff");
+        if(!histDiff) { printf("ERROR : histDiff not found\n"); return; }
+
+        histBarlow->SetMaximum(10.0);
+
+        StyleHist(histDiff, colors[iFile+1], kOpenCircle);
+        StyleHist(histBarlow, colors[iFile+1], kOpenCircle);
+
+        leg->AddEntry(histDiff,sInputFile.Data(),"pl");
+
+        listDiff->Add(histDiff);
+        listBarlow->Add(histBarlow);
+      }
+
+      // Int_t nPnt  = listDiff->GetEntries() ; // number of points
+      // Int_t nnCol = gStyle->GetNumberOfColors();
+
+      // processing lists
+      TH1D* hSum = ProcessList(listDiff,listBarlow,dCutBarlow);
+      StyleHist(hSum,colors[0],kDot);
+
+
+      if(hSum->GetMaximum() > 1.0) { hSum->SetMaximum(1.0); }
+      // hSum->SetMaximum(1.0);
+      leg->AddEntry(hSum,"Sum in quad.","l");
+
+      TCanvas* can = new TCanvas("can","can",1500,500);
+      can->Divide(3,1);
+      can->cd(1);
+      leg->Draw();
+      can->cd(2);
+      hSum->Draw();
+      for(Int_t i(0); i < listDiff->GetEntries(); ++i) { ((TH1D*) listDiff->At(i))->DrawCopy("same"); }
+
+      can->cd(3);
+      ((TH1D*) listBarlow->At(0))->Draw("hist");
+      for(Int_t i(1); i < listBarlow->GetEntries(); ++i) { ((TH1D*) listBarlow->At(i))->DrawCopy("same  hist"); }
+
+
+      can->SaveAs(Form("%s/syst_%s_cent%d.pdf",sOutPath.Data(),sSpecies[iSpec].Data(), iCent));
+      fileOut->cd();
+      hSum->Write(Form("hSum_cent%d",iCent));
+    }
+
+
+  }
+
+  return;
+}
+// ==================================================================================================================
+TH1D* ProcessList(TList* listDiff, TList* listBarlow, Double_t dCut)
+{
+  if(!listDiff) { printf("ERROR-ProcessList : listDiff not found!\n"); return 0x0; }
+  if(!listBarlow) { printf("ERROR-ProcessList : listBarlow not found!\n"); return 0x0; }
+
+  TH1D* hSum = (TH1D*) listDiff->At(0)->Clone("hSum");
+
+  for(Int_t iEntry(1); iEntry < listDiff->GetEntries(); ++iEntry)
+  {
+    // 0 already done;
+    TH1D* hNew = (TH1D*) listDiff->At(iEntry);
+
+    TH1D* hBarlow = (TH1D*) listBarlow->At(iEntry);
+
+    for(Int_t bin(1); bin < hSum->GetNbinsX()+1; ++bin)
+    {
+      Double_t dBarlow = hBarlow->GetBinContent(bin);
+
+      if(dCut > 0.0 && dBarlow < dCut) continue;
+
+      Double_t dOldCont = hSum->GetBinContent(bin);
+      // Double_t dOldErr = hSum->GetBinError(bin);
+
+      Double_t dNewCont = hNew->GetBinContent(bin);
+      // Double_t dNewErr = 0.0;
+
+      hSum->SetBinContent(bin,TMath::Sqrt(dOldCont*dOldCont + dNewCont*dNewCont));
+      hSum->SetBinError(bin,0.0);
+      // hSum->SetBinError(bin,TMath::Sqrt(dNewErrSq));
+
+    }
+  }
+
+  return hSum;
+}
+// ==================================================================================================================
+void StyleHist(TH1* hist, Color_t color, Style_t markerStyle, Bool_t showStats)
+{
+  if(!hist) { printf("ERROR-DrawHist: Hist does not found.\n"); return; }
+  hist->SetStats(showStats);
+  hist->SetLineColor(color);
+  hist->SetMarkerColor(color);
+  hist->SetMarkerStyle(markerStyle);
+  return;
+};
+// ==================================================================================================================
+void SetCustomPalette()
+{
+  // # Usage for getting the color out of pallete
+  // Int_t nPnt  = 2 ; // number of points
+  // Int_t nnCol = gStyle->GetNumberOfColors();
+  // Int_t iLegInx = 0;
+  // for(Int_t iMult(iMultMin); iMult < 2; ++iMult)
+  // {
+  //   Int_t idx = loop * Float_t(nnCol-1) / (nPnt-1);
+  //   StyleHist(histUnit, gStyle->GetColorPalette(idx), kOpenCircle);
+  // }
+  // ==================================================================================================================
+
+  Double_t stops[9] = { 0.0000, 0.1250, 0.2500, 0.3750, 0.5000,	0.6250, 0.7500, 0.8750, 1.0000};
+
+
+  // Rain Bow
+  // case 55:
+  // Double_t red[9]   = {  0./255.,   5./255.,  15./255.,  35./255., 102./255., 196./255., 208./255., 199./255., 110./255.};
+  // Double_t green[9] = {  0./255.,  48./255., 124./255., 192./255., 206./255., 226./255.,  97./255.,  16./255.,   0./255.};
+  // Double_t blue[9]  = { 99./255., 142./255., 198./255., 201./255.,  90./255.,  22./255.,  13./255.,   8./255.,   2./255.};
+
+  // Bird
+  //case 57:
+  // Double_t red[9]   = { 0.2082, 0.0592, 0.0780, 0.0232, 0.1802, 0.5301, 0.8186, 0.9956, 0.9764};
+  // Double_t green[9] = { 0.1664, 0.3599, 0.5041, 0.6419, 0.7178, 0.7492, 0.7328, 0.7862, 0.9832};
+  // Double_t blue[9]  = { 0.5293, 0.8684, 0.8385, 0.7914, 0.6425, 0.4662, 0.3499, 0.1968, 0.0539};
+
+  // Blue Green Yellow
+  //case 71:
+  // Double_t red[9]   = { 22./255., 19./255.,  19./255.,  25./255.,  35./255.,  53./255.,  88./255., 139./255., 210./255.};
+  // Double_t green[9] = {  0./255., 32./255.,  69./255., 108./255., 135./255., 159./255., 183./255., 198./255., 215./255.};
+  // Double_t blue[9]  = { 77./255., 96./255., 110./255., 116./255., 110./255., 100./255.,  90./255.,  78./255.,  70./255.};
+
+  // Solar
+  // case 100:
+  Double_t red[9]   = { 99./255., 116./255., 154./255., 174./255., 200./255., 196./255., 201./255., 201./255., 230./255.};
+  Double_t green[9] = {  0./255.,   0./255.,   8./255.,  32./255.,  58./255.,  83./255., 119./255., 136./255., 173./255.};
+  Double_t blue[9]  = {  5./255.,   6./255.,   7./255.,   9./255.,   9./255.,  14./255.,  17./255.,  19./255.,  24./255.};
+
+  // Viridis
+  // case 112:
+  // Double_t red[9]   = { 26./255., 51./255.,  43./255.,  33./255.,  28./255.,  35./255.,  74./255., 144./255., 246./255.};
+  // Double_t green[9] = {  9./255., 24./255.,  55./255.,  87./255., 118./255., 150./255., 180./255., 200./255., 222./255.};
+  // Double_t blue[9]  = { 30./255., 96./255., 112./255., 114./255., 112./255., 101./255.,  72./255.,  35./255.,   0./255.};
+
+  // Cividis
+  // case 113:
+  // Double_t red[9]   = {  0./255.,   5./255.,  65./255.,  97./255., 124./255., 156./255., 189./255., 224./255., 255./255.};
+  // Double_t green[9] = { 32./255.,  54./255.,  77./255., 100./255., 123./255., 148./255., 175./255., 203./255., 234./255.};
+  // Double_t blue[9]  = { 77./255., 110./255., 107./255., 111./255., 120./255., 119./255., 111./255.,  94./255.,  70./255.};
+
+  Int_t pal = TColor::CreateGradientColorTable(9, stops, red, green, blue, 255, 1);
+  const Int_t nCol = 255;
+  Int_t colors[nCol];
+  for (int i=0; i<nCol; i++) colors[i] =pal+i;
+
+  gStyle->SetPalette(nCol,colors);
+}
---
+++ uniFlow/macros/systematics_qm18/ProcessSyst.C
@@ -0,0 +1,341 @@
+#include "TFile.h"
+#include "TH1.h"
+#include "TList.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TLine.h"
+#include "TLegend.h"
+#include "TMath.h"
+#include "TSystem.h"
+
+TH1D* DivideHistos(TH1* nom, TH1* denom, Bool_t bCor = kFALSE);
+TH1D* BarlowTest(TH1* nom, TH1* denom);
+TH1D* Diff(TH1* ratio);
+void StyleHist(TH1* hist, Color_t color = kRed, Style_t markerStyle = kOpenCircle, Bool_t showStats = kFALSE);
+TList* PrepareCanvas(TCanvas* can);
+
+Color_t colBase = kRed;
+Color_t colSyst = kBlue+2;
+
+Int_t markBase = kOpenSquare;
+Int_t markSyst = kFullCircle;
+
+void ProcessSyst()
+{
+  TString sGap = "gap04";
+
+  // TString sType = "tracking"; TString sTag[] = {"FB","PV","cls"}; TString sSpecies[] = {"Charged","Pion","Kaon","Proton","K0s","Lambda","Phi"};
+  TString sType = "pid"; TString sTag[] = {"3sigma","bayes90"}; TString sSpecies[] = {"Pion","Kaon","Proton","Phi"};
+  // TString sType = "v0s"; TString sTag[] = {"3sigma","decayRad","DCAdaughters","CPA"}; TString sSpecies[] = {"K0s","Lambda"};
+  Int_t iNumTags = sizeof(sTag)/sizeof(sTag[0]);
+  Int_t iNumSpecies = sizeof(sSpecies)/sizeof(sSpecies[0]);
+
+  TString sInputBase = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/merged-pPb-16qt-nua/subt/output_binning-3/"+sGap+"/Subtracted.root";
+  TString sOutputPath = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/syst/out-binning-3/"+sType+"/";
+
+  TString sCentLabels[] = {"0-10%","10-20%","20-40%","40-60%","60-100%"};
+  Int_t iNumCent = sizeof(sCentLabels)/sizeof(sCentLabels[0]);
+  // Int_t iNumCent = 1;
+
+  //  --------------------------------------------------------
+
+  for(Int_t iTag(0); iTag < iNumTags; ++iTag)
+  {
+    TString sInputSyst = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/syst/"+sType+"/merged-16qt/subt/output_binning-3/"+sGap+"/"+sTag[iTag]+"/Subtracted.root";
+
+    TFile* fileInputBase = TFile::Open(sInputBase.Data(),"READ");
+    if(!fileInputBase) { printf("ERROR : fileInputBase not found!\n"); return; }
+    TFile* fileInputSyst = TFile::Open(sInputSyst.Data(),"READ");
+    if(!fileInputSyst) { printf("ERROR : fileInputSyst not found!\n"); return; }
+
+    gSystem->mkdir(sOutputPath.Data(),kTRUE);
+    TFile* fileOutput = TFile::Open(Form("%s/%s.root",sOutputPath.Data(),sTag[iTag].Data()),"RECREATE");
+    if(!fileOutput) { printf("ERROR : fileOutput not found!\n"); return; }
+
+    for(Int_t iSpecies(0); iSpecies < iNumSpecies; ++iSpecies)
+    {
+      for(Int_t iCent(0); iCent < iNumCent; ++iCent)
+      {
+        TString sHistName = Form("hFlow2_%s_harm2_%s_cent%d",sSpecies[iSpecies].Data(),sGap.Data(),iCent);
+
+        TH1D* histBase = (TH1D*) fileInputBase->Get(sHistName.Data());
+        if(!histBase) { printf("ERROR : Input histBase '%s' does not found\n",sHistName.Data()); fileInputBase->ls(); return; }
+        StyleHist(histBase,colBase,markBase);
+
+        TH1D* histSyst = (TH1D*) fileInputSyst->Get(sHistName.Data());
+        if(!histSyst) { printf("ERROR : Input histSyst '%s' does not found\n",sHistName.Data()); fileInputSyst->ls(); return; }
+        StyleHist(histSyst,colSyst,markSyst);
+
+        TH1D* histRatio = DivideHistos(histSyst,histBase,kTRUE);
+        if(!histRatio) { printf("ERROR: Ratio failed\n"); return; }
+        StyleHist(histRatio,colSyst,markSyst);
+
+        TH1D* histDiff = Diff(histRatio);
+        if(!histDiff) { printf("ERROR: Diff failed\n"); return; }
+        StyleHist(histRatio,colSyst,markSyst);
+
+        TH1D* histBarlow = BarlowTest(histBase,histSyst);
+        if(!histBarlow) { printf("ERROR : Barlow failed\n"); return; }
+        StyleHist(histBarlow,colSyst,markSyst);
+
+        TList* listOut = new TList();
+
+        histBase->SetNameTitle("histBase",Form("%s (%s): %s; p_{T} (GeV/c); v_{2}{2,%s}",sSpecies[iSpecies].Data(),sCentLabels[iCent].Data(),sTag[iTag].Data(),sGap.Data()));
+        histSyst->SetNameTitle("histSyst",Form("%s (%s): %s; p_{T} (GeV/c); v_{2}{2,%s}",sSpecies[iSpecies].Data(),sCentLabels[iCent].Data(),sTag[iTag].Data(),sGap.Data()));
+        histRatio->SetNameTitle("histRatio",Form("Ratio x_{%s} / x_{default} ; p_{T} (GeV/c); default / %s",sTag[iTag].Data(),sTag[iTag].Data()));
+        histDiff->SetNameTitle("histDiff",Form("| x_{%s} / x_{default} - 1|; p_{T} (GeV/c); Diff",sTag[iTag].Data()));
+        histBarlow->SetNameTitle("histBarlow",Form("Barlow | x_{%s} - x_{default} | / #sqrt{#sigma^{2}_{syst} - #sigma^{2}_{default}}; p_{T} (GeV/c); Barlow",sTag[iTag].Data()));
+        listOut->Add(histBase);
+        listOut->Add(histSyst);
+        listOut->Add(histRatio);
+        listOut->Add(histDiff);
+        listOut->Add(histBarlow);
+
+        fileOutput->cd();
+        listOut->Write(Form("%s_cent%d",sSpecies[iSpecies].Data(),iCent),TObject::kSingleKey);
+
+        TLegend* leg = new TLegend(0.55,0.15,0.88,0.38);
+        leg->SetBorderSize(0);
+        leg->SetFillColorAlpha(0,0);
+        leg->SetHeader(Form("%s (%s)",sSpecies[iSpecies].Data(),sCentLabels[iCent].Data()));
+        leg->AddEntry(histBase,"Default","pel");
+        leg->AddEntry(histSyst,sTag[iTag].Data(),"pel");
+
+        TLine* lineUnity = new TLine();
+        lineUnity->SetLineStyle(2);
+        lineUnity->SetLineColor(kGray+1);
+
+        TCanvas* can = new TCanvas("can","can",1800,400);
+        can->Divide(4,1);
+        can->cd(1);
+        // TH1* frame_main = (TH1*) gPad->DrawFrame(0.0,0.0,7.0,0.5);
+        gPad->SetBottomMargin(0.13);
+        gPad->SetRightMargin(0.03);
+        gPad->SetLeftMargin(0.15);
+        histSyst->SetLabelFont(43,"XY");
+        histSyst->SetLabelSize(14,"XY");
+        histSyst->SetTitleFont(43,"XY");
+        histSyst->SetTitleSize(14,"XY");
+        histSyst->SetTitleOffset(2.1,"Y");
+        histSyst->SetTitleOffset(1.5,"X");
+
+        histSyst->DrawCopy();
+        histBase->DrawCopy("same");
+        leg->Draw();
+
+        can->cd(2);
+        // TH1* frame_ratio = (TH1*) gPad->DrawFrame(0.0,0.5,7.0,1.5);
+        gPad->SetBottomMargin(0.13);
+        gPad->SetRightMargin(0.03);
+        gPad->SetLeftMargin(0.15);
+        histRatio->SetLabelFont(43,"XY");
+        histRatio->SetLabelSize(14,"XY");
+        histRatio->SetTitleFont(43,"XY");
+        histRatio->SetTitleSize(14,"XY");
+        histRatio->SetTitleOffset(2.1,"Y");
+        histRatio->SetTitleOffset(1.5,"X");
+
+        histRatio->Draw();
+        lineUnity->DrawLine(histRatio->GetXaxis()->GetXmin(),1.0,histRatio->GetXaxis()->GetXmax(),1.0);
+
+        can->cd(3);
+        gPad->SetBottomMargin(0.13);
+        gPad->SetRightMargin(0.03);
+        gPad->SetLeftMargin(0.15);
+        histDiff->SetLabelFont(43,"XY");
+        histDiff->SetLabelSize(14,"XY");
+        histDiff->SetTitleFont(43,"XY");
+        histDiff->SetTitleSize(14,"XY");
+        histDiff->SetTitleOffset(2.1,"Y");
+        histDiff->SetTitleOffset(1.5,"X");
+        histDiff->DrawCopy();
+
+        can->cd(4);
+        gPad->SetBottomMargin(0.13);
+        gPad->SetRightMargin(0.03);
+        gPad->SetLeftMargin(0.15);
+        histBarlow->SetLabelFont(43,"XY");
+        histBarlow->SetLabelSize(14,"XY");
+        histBarlow->SetTitleFont(43,"XY");
+        histBarlow->SetTitleSize(14,"XY");
+        histBarlow->SetTitleOffset(2.1,"Y");
+        histBarlow->SetTitleOffset(1.5,"X");
+        histBarlow->DrawCopy();
+
+
+        TString sOut = Form("%s/plots_%s",sOutputPath.Data(),sTag[iTag].Data());
+        gSystem->mkdir(sOut.Data(),kTRUE);
+        can->SaveAs(Form("%s/%s_cent%d.pdf",sOut.Data(),sSpecies[iSpecies].Data(),iCent));
+      }
+    }
+  }
+
+
+
+
+  return;
+}
+// ==================================================================================================================
+void StyleHist(TH1* hist, Color_t color, Style_t markerStyle, Bool_t showStats)
+{
+  if(!hist) { printf("ERROR-DrawHist: Hist does not found.\n"); return; }
+  hist->SetStats(showStats);
+  hist->SetLineColor(color);
+  hist->SetMarkerColor(color);
+  hist->SetMarkerStyle(markerStyle);
+  return;
+};
+// ==================================================================================================================
+TH1D* DivideHistos(TH1* nom, TH1* denom, Bool_t bCor)
+{
+  if(!nom || !denom) { printf("ERR: either of the histos does not exists\n"); return 0x0; }
+
+  Int_t binsNom = nom->GetNbinsX();
+  Int_t binsDenom = denom->GetNbinsX();
+
+  // if(binsNom != binsDenom) { printf("ERR: Different # of bins\n"); return 0x0; }
+
+  TH1D* ratio = (TH1D*) nom->Clone(Form("Ratio_%s_%s",nom->GetName(),denom->GetName()));
+  ratio->Reset();
+
+  Double_t dContNom = 0, dErrNom = 0;
+  Double_t dContDenom = 0, dErrDenom = 0;
+  Double_t dContRatio = 0, dErrRatio = 0;
+  for(Short_t iBinNom(1); iBinNom < binsNom+1; iBinNom++)
+  {
+    Double_t dBinCenter = nom->GetBinCenter(iBinNom);
+    Int_t iBinDenom = denom->FindFixBin(dBinCenter);
+
+    dContNom = nom->GetBinContent(iBinNom);
+    dErrNom = nom->GetBinError(iBinNom);
+    dContDenom = denom->GetBinContent(iBinDenom);
+    dErrDenom = denom->GetBinError(iBinDenom);
+
+    if(dContDenom == 0.0) continue;
+
+    dContRatio =  dContNom / dContDenom;
+    dErrRatio = TMath::Power(dErrNom/dContDenom, 2) + TMath::Power( dErrDenom*dContNom/(dContDenom*dContDenom), 2);
+    // printf("Err (before) : %g | ", TMath::Sqrt(dErrRatio));
+
+    if(bCor) dErrRatio -= (2*dContNom*dErrDenom*dErrNom/TMath::Power(dContDenom,3));
+    // printf("(after) : %g\n", TMath::Sqrt(dErrRatio));
+
+    ratio->SetBinContent(iBinNom,dContRatio);
+    ratio->SetBinError(iBinNom,TMath::Sqrt(dErrRatio));
+  }
+
+  return ratio;
+}
+// ==================================================================================================================
+TH1D* Diff(TH1* ratio)
+{
+  if(!ratio) { printf("Diff-ERROR: ratio histo does not exists\n"); return 0x0; }
+
+  TH1D* hDiff = (TH1D*) ratio->Clone(Form("Diff_%s",ratio->GetName()));
+  hDiff->Reset();
+
+  for(Int_t bin(1); bin < hDiff->GetNbinsX()+1; ++bin)
+  {
+    Double_t dCont = ratio->GetBinContent(bin);
+    Double_t dErr = ratio->GetBinError(bin);
+
+    hDiff->SetBinContent(bin, TMath::Abs(dCont-1.0));
+    hDiff->SetBinError(bin,dErr);
+  }
+
+  hDiff->SetMinimum(0.0);
+
+  return hDiff;
+}
+// ==================================================================================================================
+TH1D* BarlowTest(TH1* nom, TH1* denom)
+{
+  if(!nom || !denom) { printf("ERR: either of the histos does not exists\n"); return 0x0; }
+
+  Int_t binsNom = nom->GetNbinsX();
+  Int_t binsDenom = denom->GetNbinsX();
+
+  // if(binsNom != binsDenom) { printf("ERR: Different # of bins\n"); return 0x0; }
+
+  TH1D* hBarlow = (TH1D*) nom->Clone(Form("Barlow_%s_%s",nom->GetName(),denom->GetName()));
+  hBarlow->Reset();
+
+  Double_t dContNom = 0, dErrNom = 0;
+  Double_t dContDenom = 0, dErrDenom = 0;
+  Double_t dContRatio = 0, dErrRatio = 0;
+  for(Short_t iBinNom(1); iBinNom < binsNom+1; iBinNom++)
+  {
+    Double_t dBinCenter = nom->GetBinCenter(iBinNom);
+    Int_t iBinDenom = denom->FindFixBin(dBinCenter);
+
+    dContNom = nom->GetBinContent(iBinNom);
+    dErrNom = nom->GetBinError(iBinNom);
+    dContDenom = denom->GetBinContent(iBinDenom);
+    dErrDenom = denom->GetBinError(iBinDenom);
+
+
+    Double_t dBarlowNom = TMath::Abs(dContDenom-dContNom);
+    Double_t dBarlowDenom = TMath::Abs(dErrDenom*dErrDenom - dErrNom*dErrNom);
+    // Double_t dBarlowDenom = dErrDenom*dErrDenom - dErrNom*dErrNom;
+
+    if(dBarlowDenom <= 0.0) continue;
+
+    hBarlow->SetBinContent(iBinDenom,dBarlowNom/TMath::Sqrt(dBarlowDenom));
+  }
+
+  hBarlow->SetMinimum(0.0);
+
+  return hBarlow;
+}
+// ==================================================================================================================
+TList* PrepareCanvas(TCanvas* can)
+{
+  if(!can) { printf("ERROR-PrepareCanvas: no canvas found!\n"); return 0x0; }
+
+  TLine* lUnity = new TLine();
+  lUnity->SetLineColor(kGray+1);
+  lUnity->SetLineStyle(kDashed);
+
+  can->cd();
+  TPad* padMain = new TPad("padMain","padMain", 0, 0.3, 1, 1.0);
+  padMain->SetBottomMargin(0.0);
+  padMain->SetRightMargin(0.03);
+  padMain->SetLeftMargin(0.13);
+  padMain->Draw();
+  padMain->cd();
+  TH1* frame_canDiff_1 = (TH1*) gPad->DrawFrame(0.0,-0.05,4.0,0.4,Form("; p_{T} (GeV/c); v_{2}{2,|#Delta#eta|>0.8}"));
+  frame_canDiff_1->SetTitleFont(43,"XY");
+  frame_canDiff_1->SetTitleSize(18,"XY");
+  frame_canDiff_1->SetTitleOffset(4.3,"X");
+  frame_canDiff_1->SetLabelFont(43,"X");
+  frame_canDiff_1->SetLabelSize(18,"X");
+  // frame_canDiff_1->SetTitleFont(43,"Y");
+  frame_canDiff_1->SetTitleOffset(2.2,"Y");
+
+  can->cd();
+  TPad* padRatio = new TPad("padRatio","padRatio", 0, 0.0, 1, 0.3);
+  padRatio->SetTopMargin(0.0);
+  padRatio->SetBottomMargin(0.25);
+  padRatio->SetRightMargin(0.03);
+  padRatio->SetLeftMargin(0.13);
+  padRatio->Draw();
+  padRatio->cd();
+  TH1* frame_canDiff_2 = (TH1*) gPad->DrawFrame(0.0,0.65,4.0,1.35);
+  frame_canDiff_2->SetTitle(Form("; p_{T} (GeV/c); Kch / K0s   "));
+  frame_canDiff_2->SetNdivisions(505,"Y");
+  frame_canDiff_2->SetTitleFont(43,"XY");
+  frame_canDiff_2->SetTitleSize(18,"XY");
+  frame_canDiff_2->SetTitleOffset(4.3,"X");
+  frame_canDiff_2->SetTitleOffset(2.2,"Y");
+  frame_canDiff_2->SetLabelFont(43,"XY");
+  frame_canDiff_2->SetLabelSize(18,"XY");
+  lUnity->DrawLine(0.0,1.0,4.0,1.0);
+
+  TList* list = new TList();
+  list->Add(padMain);
+  list->Add(padRatio);
+  list->Add(frame_canDiff_1);
+  list->Add(frame_canDiff_2);
+
+  return list;
+}
