88b95a23e7bad294fae8d25dd7e477f3c95bbb6d
Vojtech Pacik <vojtech.pacik@cern.ch>
2018-06-28 13:29:59 +0200

    PlotNCQscaleAfterSub.C

--- uniFlow/macros/plotting/PlotNCQscaleAfterSub.C
+++ uniFlow/macros/plotting/PlotNCQscaleAfterSub.C
@@ -25,14 +25,22 @@ TGraphErrors* ScaleByNCQ(TGraphErrors* points, Bool_t bScaleX, Bool_t bMeson, Do
 void StyleHist(TH1* hist, Color_t color = kRed, Style_t markerStyle = kOpenCircle);
 void SetStyle(Bool_t graypalette=kFALSE);
 
-TString sInputPathTop = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/merged-pPb-16qt-nua/subt/output_binning-3/gap04/";
-TString sOutputPath = Form("%s/results-ncq/",sInputPathTop.Data());
+TString sInputPathTop = "/Users/vpacik/NBI/Flow/uniFlow/results/qm-run/merged-pPb-16qt-nua/subt/output_binning-3-0204060100/gap04/";
+TString sOutputPath = Form("%s/results-ncq-4/",sInputPathTop.Data());
 
 // // Original
-TString sSpecies_labels[] = {"#pi^{#pm}","K^{#pm}","K^{0}_{S}","p(#bar{p})","#varphi","#Lambda(#bar{#Lambda})"};
+TString sSpecies_labels[] = {" #pi^{#pm}"," K^{#pm}"," K^{0}_{S}"," p(#bar{p})"," #phi"," #Lambda(#bar{#Lambda})"};
 TString sSpecies_list[] = {"Pion","Kaon","K0s","Proton","Phi","Lambda"};
 Bool_t bMeson_list[] = {1,1,1,0,1,0};
 Int_t iPDG_codes[] = {211, 321, 310, 2212, 333, 3122};
+
+Color_t colors[] = {kRed, kBlue, kCyan+2, kGreen+2, kMagenta+1, kOrange-1};
+Color_t colorsFill[] = {kRed, kBlue, kCyan+2, kGreen+2, kMagenta+1, kOrange-1};
+
+Double_t markersSize[] = {1.2,1.1,1.1,1.0,1.2,1.2};
+
+Double_t dAlpha = 0.35;
+
 
 // W/O Phi
 // TString sSpecies_labels[] = {"#pi^{#pm}","K^{#pm}","K^{0}_{S}","p(#bar{p})","#Lambda(#bar{#Lambda})"};
@@ -40,7 +48,8 @@ Int_t iPDG_codes[] = {211, 321, 310, 2212, 333, 3122};
 // Bool_t bMeson_list[] = {1,1,1,0,0};
 // Int_t iPDG_codes[] = {211, 321, 310, 2212, 3122};
 
-TString sCent[] = {"0-10","10-20","20-40","40-60"};
+TString sCent[] = {"0-20","20-40","40-60"};
+// TString sCent[] = {"0-10","10-20","20-40","40-60"};
 
 
 Int_t iNumCent = sizeof(sCent) / sizeof(sCent[0]);
@@ -55,21 +64,23 @@ void PlotNCQscaleAfterSub()
 
   for(Int_t iCent(0); iCent < iNumCent; ++iCent)
   {
-    TCanvas* canPlot = new TCanvas("canPlot","canPlot",1500,500);
-    canPlot->Divide(3,1);
-    canPlot->cd(1);
-    TH1* frame_canPlot_1 = (TH1*) gPad->DrawFrame(0,-0.02,7.0,0.15);
-    frame_canPlot_1->SetTitle(Form("; p_{T} (GeV/c); v_{2}^{sub} {2, |#Delta#eta| > 0.4} / NCQ" ));
-    canPlot->cd(2);
-    TH1* frame_canPlot_2 = (TH1*) gPad->DrawFrame(0,-0.02,4.0,0.15);
-    frame_canPlot_2->SetTitle(Form("; p_{T} / NCQ (GeV/c); v_{2}^{sub} {2, |#Delta#eta| > 0.4} / NCQ"));
-    canPlot->cd(3);
-    TH1* frame_canPlot_3 = (TH1*) gPad->DrawFrame(0,-0.02,4.0,0.15);
-    frame_canPlot_3->SetTitle(Form("; (m_{T}-m_{0}) / NCQ (GeV/c); v_{2}^{sub} {2, |#Delta#eta| > 0.4} / NCQ"));
+    TCanvas* canPlot = new TCanvas("canPlot","canPlot",800,800);
+    TCanvas* canPlot2 = new TCanvas("canPlot2","canPlot2",800,800);
 
-    TLegend* leg = new TLegend(0.18,0.62,0.45,0.80);
+    canPlot->cd();
+    TH1* frame_canPlot_2 = (TH1*) gPad->DrawFrame(0,0.0,3.5,0.13);
+    frame_canPlot_2->SetTitle(Form("; #it{p}_{T} / #it{n}_{q} (GeV/#it{c}); v_{2}^{sub} {2, |#Delta#eta| > 0.4} / #it{n}_{q}"));
+
+    canPlot2->cd();
+    TH1* frame_canPlot_3 = (TH1*) gPad->DrawFrame(0,0.0,3.5,0.13);
+    frame_canPlot_3->SetTitle(Form("; (#it{m}_{T} - #it{m}_{0}) / #it{n}_{q} (GeV/#it{c}); v_{2}^{sub} {2, |#Delta#eta| > 0.4} / #it{n}_{q}"));
+
+    // TLegend* leg = new TLegend(0.18,0.55,0.45,0.80);
+    // TLegend* leg = new TLegend(0.18,0.5,0.4,0.77);
+    TLegend* leg = new TLegend(0.18,0.49,0.4,0.72);
     leg->SetFillColorAlpha(0,0);
     leg->SetBorderSize(0);
+    leg->SetTextSize(0.03);
 
 
     for(Int_t i(0); i < iNumSpecies; ++i)
@@ -82,40 +93,72 @@ void PlotNCQscaleAfterSub()
       TGraphErrors* histPoints = (TGraphErrors*) fileIn->Get(Form("graphPoints_%s_cent%d",sSpecies.Data(),iCent));
       if(!histPoints) { printf("No histPoints %s cent%d\n",sSpecies.Data(), iCent); return; }
 
+      TGraphErrors* histSyst = (TGraphErrors*) fileIn->Get(Form("graphSyst_%s_cent%d",sSpecies.Data(),iCent));
+      if(!histSyst) { printf("No histSyst %s cent%d\n",sSpecies.Data(), iCent); return; }
+
+      histSyst->SetFillColorAlpha(colors[i],dAlpha);
+
+
       TGraphErrors* histPoints_NCQ_v2only = ScaleByNCQ(histPoints,kFALSE,bMeson);
       if(!histPoints_NCQ_v2only) { printf("No histPoints_NCQ_v2only %s cent%d\n",sSpecies.Data(), iCent); return; }
 
       TGraphErrors* histPoints_NCQ = ScaleByNCQ(histPoints,kTRUE,bMeson);
       if(!histPoints_NCQ) { printf("No histPoints_NCQ\n"); return; }
 
+      TGraphErrors* histSyst_NCQ = ScaleByNCQ(histSyst,kTRUE,bMeson);
+      if(!histSyst_NCQ) { printf("No histSyst_NCQ\n"); return; }
+
       TGraphErrors* histPoints_NCQ_kt = ScaleByNCQ(histPoints,kTRUE,bMeson,dMass);
       if(!histPoints_NCQ_kt) { printf("No histPoints_NCQ_kt\n"); return; }
 
-      leg->AddEntry(histPoints,sSpecies.Data(),"p");
+      TGraphErrors* histSyst_NCQ_kt = ScaleByNCQ(histSyst,kTRUE,bMeson,dMass);
+      if(!histSyst_NCQ_kt) { printf("No histSyst_NCQ\n"); return; }
 
-      canPlot->cd(1);
-      histPoints_NCQ_v2only->Draw("same pZ");
 
-      canPlot->cd(2);
+      if(i == 4 && iCent == iNumCent-1) {continue;}
+
+      leg->AddEntry(histSyst,sSpecies_labels[i].Data(),"pf");
+
+      canPlot->cd();
+      histSyst_NCQ->Draw("same p2");
       histPoints_NCQ->Draw("same Pz");
 
-      canPlot->cd(3);
+      canPlot2->cd();
+      histSyst_NCQ_kt->Draw("same P2");
       histPoints_NCQ_kt->Draw("same Pz");
+
     }
 
-    canPlot->cd(1);
     TLatex * text = new TLatex();
     TLatex * text2 = new TLatex();
-    text->SetTextSizePixels(20);
     text->SetTextFont(42);
-    text2->SetTextSizePixels(20);
+    text->SetTextSize(0.04);
+    text2->SetTextFont(42);
+    text2->SetTextSize(0.04);
 
+    canPlot->cd();
+    // leg->SetHeader(Form("|#eta| < 0.8 (%s%% V0A)", sCent[iCent].Data()));
     leg->Draw("same");
-    text->DrawLatexNDC(0.186,0.85,"p-Pb #sqrt{#it{s}_{NN}} = 5.02 TeV");
-    text2->DrawLatexNDC(0.186,0.81,Form("Multiplicity class %s%% (V0A)",sCent[iCent].Data()));
+    text2->DrawLatexNDC(0.186,0.84,Form("ALICE Preliminary"));
+    // text->DrawLatexNDC(0.186,0.79,Form("p-Pb #sqrt{#it{s}_{NN}} = 5.02 TeV (%s%% V0A)", sCent[iCent].Data()));
+    text->DrawLatexNDC(0.186,0.79,Form("p-Pb #sqrt{#it{s}_{NN}} = 5.02 TeV"));
+    text->DrawLatexNDC(0.186,0.74,Form("|#eta| < 0.8"));
+    text->DrawLatexNDC(0.65,0.84,Form("%s%% (V0A)", sCent[iCent].Data()));
+
+
+
+    canPlot2->cd();
+    leg->Draw("same");
+    text2->DrawLatexNDC(0.186,0.84,Form("ALICE Preliminary"));
+    text->DrawLatexNDC(0.186,0.79,Form("p-Pb #sqrt{#it{s}_{NN}} = 5.02 TeV"));
+    text->DrawLatexNDC(0.186,0.74,Form("|#eta| < 0.8"));
+    text->DrawLatexNDC(0.65,0.84,Form("%s%% (V0A)", sCent[iCent].Data()));
 
     gSystem->mkdir(sOutputPath.Data(),kTRUE);
     canPlot->SaveAs(Form("%s/PIDvn_cent%d.pdf",sOutputPath.Data(),iCent),"pdf");
+    canPlot->SaveAs(Form("%s/PIDvn_cent%d.eps",sOutputPath.Data(),iCent),"eps");
+    canPlot2->SaveAs(Form("%s/PIDvn_kt_cent%d.pdf",sOutputPath.Data(),iCent),"pdf");
+    canPlot2->SaveAs(Form("%s/PIDvn_kt_cent%d.eps",sOutputPath.Data(),iCent),"eps");
   }
 
 
@@ -164,10 +207,14 @@ TGraphErrors* ScaleByNCQ(TGraphErrors* points, Bool_t bScaleX, Bool_t bMeson, Do
       dX = dKt;
     }
 
-    if(bScaleX) { dX = dX / dNCQ; }
+    if(bScaleX)
+    {
+      dX = dX / dNCQ;
+      dXErr = dXErr / dNCQ;
+    }
 
     points_NCQ->SetPoint(i,dX, dY);
-    points_NCQ->SetPointError(i,0.0,dYErr);
+    points_NCQ->SetPointError(i,dXErr,dYErr);
   }
 
   return points_NCQ;
@@ -222,7 +269,7 @@ void SetStyle(Bool_t graypalette) {
   gStyle->SetLabelOffset(0.01,"x");
   gStyle->SetLabelColor(kBlack,"xyz");
   gStyle->SetTitleSize(0.05,"xyz");
-  gStyle->SetTitleOffset(1.25,"y");
+  gStyle->SetTitleOffset(1.4,"y");
   gStyle->SetTitleOffset(1.2,"x");
   gStyle->SetTitleFillColor(kWhite);
   gStyle->SetTextSizePixels(26);
