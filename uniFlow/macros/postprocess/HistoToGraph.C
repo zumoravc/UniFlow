aea8f3eb78f996a2b43cc363c795a13a1379315d
Vojtech Pacik <vojta.pacik@gmail.com>
2019-05-14 11:20:01 +0200

    Macro for transfering Th1* to TGraphError impl'd

---
+++ uniFlow/macros/postprocess/HistoToGraph.C
@@ -0,0 +1,37 @@
+// Macro for transfering TH1* to TGraphErrors object
+#include </Users/vpacik/Codes/ALICE/Flow/uniFlow/macros/utils/Utils.cxx>
+#include "TH1.h"
+#include "TGraphErrors.h"
+
+void SetXerrs(TGraphErrors* graph, Double_t dErrX); // Set graphs X-axis errors to Ex value
+
+TGraphErrors* HistoToGraph(TH1* histo, Double_t dErrX = -1)
+{
+    if(!histo) {
+        Utils::Error("Input histo not found!","HistoToGraph");
+        return nullptr;
+    }
+
+    TGraphErrors* err = new TGraphErrors(histo);
+    SetXerrs(err,dErrX);
+    return err;
+}
+
+void SetXerrs(TGraphErrors* graph, Double_t dErrX)
+{
+    if(!graph) {
+        Utils::Error("Input graph not found!","SetXerrs");
+        return;
+    }
+
+    if(dErrX < 0.0) {
+        Utils::Debug("Value of ErrX < 0! Skipping!",1,"SetXerrs");
+        return;
+    }
+
+    Int_t np = graph->GetN();
+    for (Int_t i(0); i < np; ++i) {
+        Double_t dErrY = graph->GetErrorY(i);
+        graph->SetPointError(i, dErrX, dErrY);
+    }
+}
