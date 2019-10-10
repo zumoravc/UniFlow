c6b2917e0c7b988f318643905d184ea31e3ea116
zumoravc <zuzana.moravcova@cern.ch>
2019-10-10 16:49:10 +0200

    small sys - pPb

--- macros/plotting/smallSys/v22pPb.C
+++ macros/plotting/smallSys/v22pPb.C
@@ -4,28 +4,28 @@ void v22pPb(){
 
   TMultiGraph *mg = new TMultiGraph();
 
-  TGraphErrors *nogap = new TGraphErrors((TH1D*) fileIn->Get("Charged_hFlow2_harm2_gap-10_cent0"));
+  TGraphErrors *nogap = new TGraphErrors((TH1D*) fileIn->Get("Kaon_hFlow2_harm2_gap-10_cent0"));
   nogap->SetMarkerStyle(kOpenSquare);
   nogap->SetMarkerColor(kRed);
   nogap->SetMarkerSize(1.);
   nogap->SetLineColor(kRed);
   mg->Add(nogap);
 
-  TGraphErrors *gap00 = new TGraphErrors((TH1D*) fileIn->Get("Charged_hFlow2_harm2_gap00_cent0"));
+  TGraphErrors *gap00 = new TGraphErrors((TH1D*) fileIn->Get("Kaon_hFlow2_harm2_gap00_cent0"));
   gap00->SetMarkerStyle(kOpenCircle);
   gap00->SetMarkerColor(kGreen+3);
   gap00->SetMarkerSize(1.);
   gap00->SetLineColor(kGreen+3);
   mg->Add(gap00);
 
-  TGraphErrors *gap08 = new TGraphErrors((TH1D*) fileIn->Get("Charged_hFlow2_harm2_gap08_cent0"));
+  TGraphErrors *gap08 = new TGraphErrors((TH1D*) fileIn->Get("Kaon_hFlow2_harm2_gap08_cent0"));
   gap08->SetMarkerStyle(kOpenDiamond);
   gap08->SetMarkerColor(kBlue);
   gap08->SetMarkerSize(1.);
   gap08->SetLineColor(kBlue);
   mg->Add(gap08);
 
-  TGraphErrors *gap10 = new TGraphErrors((TH1D*) fileIn->Get("Charged_hFlow2_harm2_gap10_cent0"));
+  TGraphErrors *gap10 = new TGraphErrors((TH1D*) fileIn->Get("Kaon_hFlow2_harm2_gap10_cent0"));
   gap10->SetMarkerStyle(kOpenStar);
   gap10->SetMarkerColor(kMagenta+2);
   gap10->SetMarkerSize(1.);
@@ -44,7 +44,7 @@ void v22pPb(){
   TLegend* leg = new TLegend(0.12,0.65,0.52,0.88);
   leg->SetBorderSize(0);
   leg->SetFillColorAlpha(0.0,0.0);
-  leg->SetHeader("ALICE experiment, pPb @ 5.02TeV, LHC16q, h^{#pm}, |#eta| < 0.8, V0A cent: 0-1%");
+  leg->SetHeader("ALICE experiment, pPb @ 5.02TeV, LHC16q, K^{#pm}, |#eta| < 0.8, V0A cent: 0-10%");
   // leg->SetHeader("ALICE experiment, pPb @ 5.02TeV, LHC16q, p/#bar{p}, |#eta| < 0.8, V0A cent: 0-1%");
   gStyle->SetLegendTextSize(0.025);
   leg->SetFillStyle(0);
@@ -57,6 +57,6 @@ void v22pPb(){
   leg->AddEntry(gap10,"v_{2}{2,|#Delta#eta|>1.0}","p");
   leg->Draw("same");
 
-  can->SaveAs("v22_pPb_h.pdf");
+  can->SaveAs("v22_pPb_K.pdf");
 
 }
